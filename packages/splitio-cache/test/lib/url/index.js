4ce227ace8316a49fc4cc6cd41886d9e0d3a57cf
Facundo Cabrera <cabrerafacundo@gmail.com>
2016-01-29 11:37:30 -0300

    [splitio-cache] Fixed URL tests

--- packages/splitio-cache/test/lib/url/index.js
+++ packages/splitio-cache/test/lib/url/index.js
@@ -1,26 +1,27 @@
 'use strict';
 
-let tape = require('tape');
-let url = require('../../../lib/url');
+var tape = require('tape');
+var url = require('../../../lib/url');
 
-let NODE_ENV = process.env.NODE_ENV;
+var NODE_ENV = process.env.NODE_ENV;
 
-tape('url for development', assert => {
+tape('url for development', function (assert) {
   process.env.NODE_ENV = 'development';
   assert.true(url('/test'), 'http://localhost:8081/api/test');
   assert.end();
 });
 
-tape('url for stage', assert => {
+tape('url for stage', function (assert) {
   process.env.NODE_ENV = 'stage';
   assert.true(url('/test'), 'https://sdk-staging.split.io/api/test');
   assert.end();
 });
 
-tape('url for production', assert => {
+tape('url for production', function (assert) {
   process.env.NODE_ENV = 'production';
   assert.true(url('/test'), 'https://sdk.split.io/api/test');
   assert.end();
 });
 
 process.env.NODE_ENV = NODE_ENV;
+//# sourceMappingURL=index.js.map
