de017bc3c7b9afa5c3c36cb8e3e525f0cf7eda4a
Facundo Cabrera <cabrerafacundo@gmail.com>
2016-02-03 13:23:32 -0300

    [splitio-browser] Updated browser API to remove the user from .isOn parameters.

--- packages/splitio-browser/lib/index.js
+++ packages/splitio-browser/lib/index.js
@@ -4,11 +4,11 @@ var engine = require('@splitsoftware/splitio');
 
 var splitio = global.splitio = {};
 
-splitio.start = function splitStartUp(authorizationKey /*: string */, userId /*: string */) {
-  return engine(authorizationKey, userId).then(function (API) {
-    splitio.isOn = API.isOn;
+splitio.start = function splitStartUp(authorizationKey /*: string */, key /*: string */) {
+  return engine(authorizationKey, key).then(function(API) {
+    splitio.isOn = API.isOn.bind(API, key);
 
-    return API;
+    return splitio;
   });
 };
 
