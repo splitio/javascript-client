47bb4139f4a535e6698ad277477e18b18622a8b2
Facundo Cabrera <cabrerafacundo@gmail.com>
2016-02-15 10:00:22 -0800

    Added script to link all the modules to simplify development testing => npm run expose

---
+++ scripts/expose.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Expose all the modules inside NPM so we could link the development versions
+# across different node versions.
+
+cd packages/splitio-engine
+npm link
+cd -
+
+cd packages/splitio-cache
+npm link
+cd -
+
+cd packages/splitio-engine
+npm link
+cd -
+
+cd packages/splitio
+npm link
+cd -
+
+cd packages/splitio-browser
+npm link
+cd -
