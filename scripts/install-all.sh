074a7dc46b50a79e0e0be1453e5040865977fe64
Facundo Cabrera <cabrerafacundo@gmail.com>
2016-03-02 10:18:10 -0300

    [all] Updated install-all.sh to prune old packages

--- scripts/install-all.sh
+++ scripts/install-all.sh
@@ -2,28 +2,35 @@
 
 cd packages/splitio-utils
 npm install
+npm prune
 cd - &> /dev/null
 
 cd packages/splitio-services
 npm install
+npm prune
 cd - &> /dev/null
 
 cd packages/splitio-metrics
 npm install
+npm prune
 cd - &> /dev/null
 
 cd packages/splitio-engine
 npm install
+npm prune
 cd - &> /dev/null
 
 cd packages/splitio-cache
 npm install
+npm prune
 cd - &> /dev/null
 
 cd packages/splitio
 npm install
+npm prune
 cd - &> /dev/null
 
 cd packages/splitio-browser
 npm install
+npm prune
 cd - &> /dev/null
