6b1d5560aa7e5a04430e0f9e1fbd91699d421260
Facundo Cabrera <cabrerafacundo@gmail.com>
2016-03-04 13:28:00 -0300

    Updated bundles

--- packages/splitio-cache/lib/ds/segmentChanges.js
+++ packages/splitio-cache/lib/ds/segmentChanges.js
@@ -1,5 +1,17 @@
 'use strict';
 
+var _stringify = require('babel-runtime/core-js/json/stringify');
+
+var _stringify2 = _interopRequireDefault(_stringify);
+
+var _keys = require('babel-runtime/core-js/object/keys');
+
+var _keys2 = _interopRequireDefault(_keys);
+
+var _typeof2 = require('babel-runtime/helpers/typeof');
+
+var _typeof3 = _interopRequireDefault(_typeof2);
+
 var _objectWithoutProperties2 = require('babel-runtime/helpers/objectWithoutProperties');
 
 var _objectWithoutProperties3 = _interopRequireDefault(_objectWithoutProperties2);
@@ -8,7 +20,12 @@ var _map = require('babel-runtime/core-js/map');
 
 var _map2 = _interopRequireDefault(_map);
 
+var _types = require('../types');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Thenable = _types.Thenable;
+
 
 var segmentChangesService = require('@splitsoftware/splitio-services/lib/segmentChanges');
 var segmentChangesRequest = require('@splitsoftware/splitio-services/lib/segmentChanges/get');
@@ -20,14 +37,22 @@ function cacheKeyGenerator(authorizationKey, segmentName) {
   return authorizationKey + '/segmentChanges/' + segmentName;
 }
 
-function segmentChangesDataSource(_ref) {
-  var authorizationKey = _ref.authorizationKey;
-  var segmentName = _ref.segmentName;
+function segmentChangesDataSource(_ref2) {
+  var authorizationKey = _ref2.authorizationKey;
+  var segmentName = _ref2.segmentName;
+
+  function _ref(_id) {
+    if (!Thenable(_id)) {
+      throw new TypeError('Function "segmentChangesDataSource" return value violates contract.\n\nExpected:\nThenable\n\nGot:\n' + _inspect(_id));
+    }
+
+    return _id;
+  }
 
   var cacheKey = cacheKeyGenerator(authorizationKey, segmentName);
   var since = cache.get(cacheKey) || -1;
 
-  return segmentChangesService(segmentChangesRequest({
+  return _ref(segmentChangesService(segmentChangesRequest({
     since: since,
     segmentName: segmentName
   })).then(function (resp) {
@@ -41,8 +66,81 @@ function segmentChangesDataSource(_ref) {
     cache.set(cacheKey, till);
 
     return segmentMutatorFactory(data);
-  }).catch(function () {/* noop */});
+  }).catch(function () {/* noop */}));
 }
 
 module.exports = segmentChangesDataSource;
+
+function _inspect(input, depth) {
+  var maxDepth = 4;
+  var maxKeys = 15;
+
+  if (depth === undefined) {
+    depth = 0;
+  }
+
+  depth += 1;
+
+  if (input === null) {
+    return 'null';
+  } else if (input === undefined) {
+    return 'void';
+  } else if (typeof input === 'string' || typeof input === 'number' || typeof input === 'boolean') {
+    return typeof input === 'undefined' ? 'undefined' : (0, _typeof3.default)(input);
+  } else if (Array.isArray(input)) {
+    if (input.length > 0) {
+      var _ret = function () {
+        if (depth > maxDepth) return {
+            v: '[...]'
+          };
+
+        var first = _inspect(input[0], depth);
+
+        if (input.every(function (item) {
+          return _inspect(item, depth) === first;
+        })) {
+          return {
+            v: first.trim() + '[]'
+          };
+        } else {
+          return {
+            v: '[' + input.slice(0, maxKeys).map(function (item) {
+              return _inspect(item, depth);
+            }).join(', ') + (input.length >= maxKeys ? ', ...' : '') + ']'
+          };
+        }
+      }();
+
+      if ((typeof _ret === 'undefined' ? 'undefined' : (0, _typeof3.default)(_ret)) === "object") return _ret.v;
+    } else {
+      return 'Array';
+    }
+  } else {
+    var keys = (0, _keys2.default)(input);
+
+    if (!keys.length) {
+      if (input.constructor && input.constructor.name && input.constructor.name !== 'Object') {
+        return input.constructor.name;
+      } else {
+        return 'Object';
+      }
+    }
+
+    if (depth > maxDepth) return '{...}';
+    var indent = '  '.repeat(depth - 1);
+    var entries = keys.slice(0, maxKeys).map(function (key) {
+      return (/^([A-Z_$][A-Z0-9_$]*)$/i.test(key) ? key : (0, _stringify2.default)(key)) + ': ' + _inspect(input[key], depth) + ';';
+    }).join('\n  ' + indent);
+
+    if (keys.length >= maxKeys) {
+      entries += '\n  ' + indent + '...';
+    }
+
+    if (input.constructor && input.constructor.name && input.constructor.name !== 'Object') {
+      return input.constructor.name + ' {\n  ' + indent + entries + '\n' + indent + '}';
+    } else {
+      return '{\n  ' + indent + entries + '\n' + indent + '}';
+    }
+  }
+}
 //# sourceMappingURL=segmentChanges.js.map
--- packages/splitio-cache/lib/ds/splitChanges.js
+++ packages/splitio-cache/lib/ds/splitChanges.js
@@ -1,5 +1,24 @@
 'use strict';
 
+var _stringify = require('babel-runtime/core-js/json/stringify');
+
+var _stringify2 = _interopRequireDefault(_stringify);
+
+var _keys = require('babel-runtime/core-js/object/keys');
+
+var _keys2 = _interopRequireDefault(_keys);
+
+var _typeof2 = require('babel-runtime/helpers/typeof');
+
+var _typeof3 = _interopRequireDefault(_typeof2);
+
+var _types = require('../types');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Thenable = _types.Thenable;
+
+
 var splitChangesService = require('@splitsoftware/splitio-services/lib/splitChanges');
 var splitChangesRequest = require('@splitsoftware/splitio-services/lib/splitChanges/get');
 
@@ -8,7 +27,15 @@ var splitMutatorFactory = require('../mutators/splitChanges');
 var since = -1;
 
 function splitChangesDataSource() {
-  return splitChangesService(splitChangesRequest({
+  function _ref(_id) {
+    if (!Thenable(_id)) {
+      throw new TypeError('Function "splitChangesDataSource" return value violates contract.\n\nExpected:\nThenable\n\nGot:\n' + _inspect(_id));
+    }
+
+    return _id;
+  }
+
+  return _ref(splitChangesService(splitChangesRequest({
     since: since
   })).then(function (resp) {
     return resp.json();
@@ -20,8 +47,81 @@ function splitChangesDataSource() {
     since = till;
 
     return splitMutatorFactory(splits);
-  }).catch(function () {/* noop */});
+  }).catch(function () {/* noop */}));
 }
 
 module.exports = splitChangesDataSource;
+
+function _inspect(input, depth) {
+  var maxDepth = 4;
+  var maxKeys = 15;
+
+  if (depth === undefined) {
+    depth = 0;
+  }
+
+  depth += 1;
+
+  if (input === null) {
+    return 'null';
+  } else if (input === undefined) {
+    return 'void';
+  } else if (typeof input === 'string' || typeof input === 'number' || typeof input === 'boolean') {
+    return typeof input === 'undefined' ? 'undefined' : (0, _typeof3.default)(input);
+  } else if (Array.isArray(input)) {
+    if (input.length > 0) {
+      var _ret = function () {
+        if (depth > maxDepth) return {
+            v: '[...]'
+          };
+
+        var first = _inspect(input[0], depth);
+
+        if (input.every(function (item) {
+          return _inspect(item, depth) === first;
+        })) {
+          return {
+            v: first.trim() + '[]'
+          };
+        } else {
+          return {
+            v: '[' + input.slice(0, maxKeys).map(function (item) {
+              return _inspect(item, depth);
+            }).join(', ') + (input.length >= maxKeys ? ', ...' : '') + ']'
+          };
+        }
+      }();
+
+      if ((typeof _ret === 'undefined' ? 'undefined' : (0, _typeof3.default)(_ret)) === "object") return _ret.v;
+    } else {
+      return 'Array';
+    }
+  } else {
+    var keys = (0, _keys2.default)(input);
+
+    if (!keys.length) {
+      if (input.constructor && input.constructor.name && input.constructor.name !== 'Object') {
+        return input.constructor.name;
+      } else {
+        return 'Object';
+      }
+    }
+
+    if (depth > maxDepth) return '{...}';
+    var indent = '  '.repeat(depth - 1);
+    var entries = keys.slice(0, maxKeys).map(function (key) {
+      return (/^([A-Z_$][A-Z0-9_$]*)$/i.test(key) ? key : (0, _stringify2.default)(key)) + ': ' + _inspect(input[key], depth) + ';';
+    }).join('\n  ' + indent);
+
+    if (keys.length >= maxKeys) {
+      entries += '\n  ' + indent + '...';
+    }
+
+    if (input.constructor && input.constructor.name && input.constructor.name !== 'Object') {
+      return input.constructor.name + ' {\n  ' + indent + entries + '\n' + indent + '}';
+    } else {
+      return '{\n  ' + indent + entries + '\n' + indent + '}';
+    }
+  }
+}
 //# sourceMappingURL=splitChanges.js.map
