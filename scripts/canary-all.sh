9425ba20ddcdf89258f23c65866e1f4afeb4ccd7
Facundo Cabrera <cabrerafacundo@gmail.com>
2016-03-04 13:35:31 -0300

    Added scripts for canary publishment

---
+++ scripts/canary-all.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+cd packages/splitio-utils
+npm publish --tag canary
+cd - &> /dev/null
+
+cd packages/splitio-services
+npm publish --tag canary
+cd - &> /dev/null
+
+cd packages/splitio-metrics
+npm publish --tag canary
+cd - &> /dev/null
+
+cd packages/splitio-cache
+npm publish --tag canary
+cd - &> /dev/null
+
+cd packages/splitio-engine
+npm publish --tag canary
+cd - &> /dev/null
+
+# cd packages/splitio
+# npm publish --tag canary
+# cd - &> /dev/null
+
+cd packages/splitio-browser
+npm publish --tag canary
+cd - &> /dev/null
