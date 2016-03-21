001fb8b975795c695ba6f5c74ec92601c3f27c86
Facundo Cabrera <cabrerafacundo@gmail.com>
2016-03-21 18:15:44 -0300

    Updated version 3.0.0

--- packages/splitio-utils/es6/settings/index.js
+++ packages/splitio-utils/es6/settings/index.js
@@ -89,7 +89,7 @@ module.exports = {
 
     switch (settingName) {
       case 'version':
-        return 'javascript-2.0.0';
+        return 'javascript-3.0.0';
       case 'core':
         return settings.core;
       case 'scheduler':
