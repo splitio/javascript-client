129656348a0863b56a79a1022b472a02ab9b8053
Facundo Cabrera <cabrerafacundo@gmail.com>
2016-03-22 16:22:27 -0300

    Updated E2E to cover attributes scenarios

--- e2e/es6/e2e.spec.js
+++ e2e/es6/e2e.spec.js
@@ -52,10 +52,173 @@ tape('E2E ', assert => {
   assert.equal(sdk.getTreatment('qc_team'), 'control', 'Ask for `qc_team` before initialization');
 
   sdk.ready().then(function () {
-    assert.equal(sdk.getTreatment('blacklist'), 'not_allowed', 'Ask for `blacklist` after initialization');
-    assert.equal(sdk.getTreatment('whitelist'), 'allowed', 'Ask for `whitelist` before initialization');
-    assert.equal(sdk.getTreatment('splitters'), 'on', 'Ask for `splitters` before initialization');
-    assert.equal(sdk.getTreatment('qc_team'), 'no', 'Ask for `qc_team` before initialization');
+    assert.equal(sdk.getTreatment('blacklist'), 'not_allowed');
+    assert.equal(sdk.getTreatment('whitelist'), 'allowed');
+    assert.equal(sdk.getTreatment('splitters'), 'on');
+    assert.equal(sdk.getTreatment('qc_team'), 'no');
+
+    assert.equal(sdk.getTreatment('employees_between_21_and_50_and_chrome'), 'off');
+    assert.equal(sdk.getTreatment('employees_between_21_and_50_and_chrome', {
+      age: 21
+    }), 'off');
+    assert.equal(sdk.getTreatment('employees_between_21_and_50_and_chrome', {
+      age: 20,
+      agent: 'chrome'
+    }), 'off');
+    assert.equal(sdk.getTreatment('employees_between_21_and_50_and_chrome', {
+      age: 21,
+      agent: 'chrome'
+    }), 'on');
+
+    assert.equal(sdk.getTreatment('user_attr_gte_10_and_user_attr2_is_not_foo'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_gte_10_and_user_attr2_is_not_foo', {
+      attr: 55,
+      attr2: 'bar'
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_gte_10_and_user_attr2_is_not_foo', {
+      attr: 55,
+      attr2: 'foo'
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_account_in_whitelist'), 'off');
+    assert.equal(sdk.getTreatment('user_account_in_whitelist', {
+      account: 'key_1@split.io'
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_account_in_whitelist', {
+      account: 'key_6@split.io'
+    }), 'off');
+
+    // This is an special case for the browser.
+    assert.equal(sdk.getTreatment('user_account_in_segment_employees'), 'off');
+    assert.equal(sdk.getTreatment('user_account_in_segment_employees', {
+      account: 'key_1@split.io'
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_account_in_segment_all'), 'off');
+    assert.equal(sdk.getTreatment('user_account_in_segment_all', {
+      account: 'something'
+    }), 'on');
+
+    assert.equal(sdk.getTreatment('user_attr_btw_datetime_1458240947021_and_1458246884077'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_btw_datetime_1458240947021_and_1458246884077', {
+      attr: 1458240947021
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_btw_datetime_1458240947021_and_1458246884077', {
+      attr: 1458240947020
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_btw_number_10_and_20'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_btw_number_10_and_20', {
+      attr: 9
+    }), 'off');
+    assert.equal(sdk.getTreatment('user_attr_btw_number_10_and_20', {
+      attr: 21
+    }), 'off');
+    assert.equal(sdk.getTreatment('user_attr_btw_number_10_and_20', {
+      attr: 15
+    }), 'on');
+
+    assert.equal(sdk.getTreatment('user_attr_btw_10_and_20'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_btw_10_and_20', {
+      attr: 9
+    }), 'off');
+    assert.equal(sdk.getTreatment('user_attr_btw_10_and_20', {
+      attr: 21
+    }), 'off');
+    assert.equal(sdk.getTreatment('user_attr_btw_10_and_20', {
+      attr: 15
+    }), 'on');
+
+    assert.equal(sdk.getTreatment('user_attr_lte_datetime_1458240947021'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_lte_datetime_1458240947021', {
+      attr: 1458240947021
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_lte_datetime_1458240947021', {
+      attr: 1458240947020
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_lte_datetime_1458240947021', {
+      attr: 1458240947022
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_lte_number_10'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_lte_number_10', {
+      attr: 9
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_lte_number_10', {
+      attr: 10
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_lte_number_10', {
+      attr: 11
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_lte_10'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_lte_10', {
+      attr: 9
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_lte_10', {
+      attr: 10
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_lte_10', {
+      attr: 11
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_gte_datetime_1458240947021'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_gte_datetime_1458240947021', {
+      attr: 1458240947021
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_gte_datetime_1458240947021', {
+      attr: 1458240947300
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_gte_datetime_1458240947021', {
+      attr: 1458240947020
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_gte_number_10'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_gte_number_10', {
+      attr: 10
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_gte_number_10', {
+      attr: 11
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_gte_number_10', {
+      attr: 0
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_gte_10'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_gte_10', {
+      attr: 10
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_gte_10', {
+      attr: 11
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_gte_10', {
+      attr: 0
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_eq_datetime_1458240947021'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_eq_datetime_1458240947021', {
+      attr: 1458240947021
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_eq_datetime_1458240947021', {
+      attr: 11
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_eq_number_ten'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_eq_number_ten', {
+      attr: 10
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_eq_number_ten', {
+      attr: 9
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_eq_ten'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_eq_ten', {
+      attr: 10
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_eq_ten', {
+      attr: 9
+    }), 'off');
+
     assert.end();
   });
 });
--- e2e/lib/e2e.spec.js
+++ e2e/lib/e2e.spec.js
@@ -1,5 +1,21 @@
 'use strict';
 
+/**
+Copyright 2016 Split Software
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**/
+
 require('@splitsoftware/splitio');
 var Split = global.splitio;
 
@@ -25,10 +41,10 @@ tape('E2E ', function (assert) {
       key: 'facundo@split.io'
     },
     scheduler: {
-      featuresRefreshRate: 1000,
-      segmentsRefreshRate: 1000,
-      metricsRefreshRate: 3000000,
-      impressionsRefreshRate: 3000000
+      featuresRefreshRate: 1,
+      segmentsRefreshRate: 1,
+      metricsRefreshRate: 3000, // for now I don't want to publish metrics during E2E run.
+      impressionsRefreshRate: 3000 // for now I don't want to publish impressions during E2E run.
     }
   });
 
@@ -38,10 +54,173 @@ tape('E2E ', function (assert) {
   assert.equal(sdk.getTreatment('qc_team'), 'control', 'Ask for `qc_team` before initialization');
 
   sdk.ready().then(function () {
-    assert.equal(sdk.getTreatment('blacklist'), 'not_allowed', 'Ask for `blacklist` after initialization');
-    assert.equal(sdk.getTreatment('whitelist'), 'allowed', 'Ask for `whitelist` before initialization');
-    assert.equal(sdk.getTreatment('splitters'), 'on', 'Ask for `splitters` before initialization');
-    assert.equal(sdk.getTreatment('qc_team'), 'no', 'Ask for `qc_team` before initialization');
+    assert.equal(sdk.getTreatment('blacklist'), 'not_allowed');
+    assert.equal(sdk.getTreatment('whitelist'), 'allowed');
+    assert.equal(sdk.getTreatment('splitters'), 'on');
+    assert.equal(sdk.getTreatment('qc_team'), 'no');
+
+    assert.equal(sdk.getTreatment('employees_between_21_and_50_and_chrome'), 'off');
+    assert.equal(sdk.getTreatment('employees_between_21_and_50_and_chrome', {
+      age: 21
+    }), 'off');
+    assert.equal(sdk.getTreatment('employees_between_21_and_50_and_chrome', {
+      age: 20,
+      agent: 'chrome'
+    }), 'off');
+    assert.equal(sdk.getTreatment('employees_between_21_and_50_and_chrome', {
+      age: 21,
+      agent: 'chrome'
+    }), 'on');
+
+    assert.equal(sdk.getTreatment('user_attr_gte_10_and_user_attr2_is_not_foo'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_gte_10_and_user_attr2_is_not_foo', {
+      attr: 55,
+      attr2: 'bar'
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_gte_10_and_user_attr2_is_not_foo', {
+      attr: 55,
+      attr2: 'foo'
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_account_in_whitelist'), 'off');
+    assert.equal(sdk.getTreatment('user_account_in_whitelist', {
+      account: 'key_1@split.io'
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_account_in_whitelist', {
+      account: 'key_6@split.io'
+    }), 'off');
+
+    // This is an special case for the browser.
+    assert.equal(sdk.getTreatment('user_account_in_segment_employees'), 'off');
+    assert.equal(sdk.getTreatment('user_account_in_segment_employees', {
+      account: 'key_1@split.io'
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_account_in_segment_all'), 'off');
+    assert.equal(sdk.getTreatment('user_account_in_segment_all', {
+      account: 'something'
+    }), 'on');
+
+    assert.equal(sdk.getTreatment('user_attr_btw_datetime_1458240947021_and_1458246884077'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_btw_datetime_1458240947021_and_1458246884077', {
+      attr: 1458240947021
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_btw_datetime_1458240947021_and_1458246884077', {
+      attr: 1458240947020
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_btw_number_10_and_20'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_btw_number_10_and_20', {
+      attr: 9
+    }), 'off');
+    assert.equal(sdk.getTreatment('user_attr_btw_number_10_and_20', {
+      attr: 21
+    }), 'off');
+    assert.equal(sdk.getTreatment('user_attr_btw_number_10_and_20', {
+      attr: 15
+    }), 'on');
+
+    assert.equal(sdk.getTreatment('user_attr_btw_10_and_20'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_btw_10_and_20', {
+      attr: 9
+    }), 'off');
+    assert.equal(sdk.getTreatment('user_attr_btw_10_and_20', {
+      attr: 21
+    }), 'off');
+    assert.equal(sdk.getTreatment('user_attr_btw_10_and_20', {
+      attr: 15
+    }), 'on');
+
+    assert.equal(sdk.getTreatment('user_attr_lte_datetime_1458240947021'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_lte_datetime_1458240947021', {
+      attr: 1458240947021
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_lte_datetime_1458240947021', {
+      attr: 1458240947020
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_lte_datetime_1458240947021', {
+      attr: 1458240947022
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_lte_number_10'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_lte_number_10', {
+      attr: 9
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_lte_number_10', {
+      attr: 10
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_lte_number_10', {
+      attr: 11
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_lte_10'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_lte_10', {
+      attr: 9
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_lte_10', {
+      attr: 10
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_lte_10', {
+      attr: 11
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_gte_datetime_1458240947021'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_gte_datetime_1458240947021', {
+      attr: 1458240947021
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_gte_datetime_1458240947021', {
+      attr: 1458240947300
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_gte_datetime_1458240947021', {
+      attr: 1458240947020
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_gte_number_10'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_gte_number_10', {
+      attr: 10
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_gte_number_10', {
+      attr: 11
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_gte_number_10', {
+      attr: 0
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_gte_10'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_gte_10', {
+      attr: 10
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_gte_10', {
+      attr: 11
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_gte_10', {
+      attr: 0
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_eq_datetime_1458240947021'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_eq_datetime_1458240947021', {
+      attr: 1458240947021
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_eq_datetime_1458240947021', {
+      attr: 11
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_eq_number_ten'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_eq_number_ten', {
+      attr: 10
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_eq_number_ten', {
+      attr: 9
+    }), 'off');
+
+    assert.equal(sdk.getTreatment('user_attr_eq_ten'), 'off');
+    assert.equal(sdk.getTreatment('user_attr_eq_ten', {
+      attr: 10
+    }), 'on');
+    assert.equal(sdk.getTreatment('user_attr_eq_ten', {
+      attr: 9
+    }), 'off');
+
     assert.end();
   });
 });
