ef78a3ce91f026322f199dcc0f5fcddd035dcbc0
Facundo Cabrera <cabrerafacundo@gmail.com>
2016-06-13 18:12:04 -0300

    Release 5.1.1

--- packages/splitio-utils/es6/settings/index.js
+++ packages/splitio-utils/es6/settings/index.js
@@ -87,7 +87,7 @@ const proto = {
   get(name) {
     switch (name) {
       case 'version':
-        return 'javascript-5.1.0';
+        return 'javascript-5.1.1';
       case 'authorizationKey':
         return this.core.authorizationKey;
       case 'key':
