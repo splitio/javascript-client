b2b1652b2bc0973af8d227e82b219f17db1fc5da
Fernando Martin <fernando@split.io>
2016-06-16 14:16:05 -0700

    new try

--- packages/splitio-engine/es6/treatments/index.js
+++ packages/splitio-engine/es6/treatments/index.js
@@ -36,7 +36,7 @@ function Treatments(ranges /*: array<number> */, treatments /*: array<string> */
     throw new RangeError('Provided invalid dataset as input');
   }
 
-  if (0) {
+  if (true == false) {
     throw new RangeError('Provided invalid dataset as input');
   }
   this._ranges = ranges;
