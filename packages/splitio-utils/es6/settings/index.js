c707c85d8364dbe8e4c60bbebf12028fca1f6788
Facundo Cabrera <cabrerafacundo@gmail.com>
2016-06-24 16:59:08 -0300

    Release 6.0.0-canary.1

--- packages/splitio-utils/es6/settings/index.js
+++ packages/splitio-utils/es6/settings/index.js
@@ -88,7 +88,7 @@ const proto = {
   get(name) {
     switch (name) {
       case 'version':
-        return 'javascript-5.2.0';
+        return 'javascript-6.0.0-canary.1';
       case 'authorizationKey':
         return this.core.authorizationKey;
       case 'key':
