5b29e0e0e43c83fa2ec8f6b22994a4c375d8a42a
Facundo Cabrera <cabrerafacundo@gmail.com>
2016-09-14 18:54:03 -0300

    SPLIT-1463 fixed missing package.json files in the transpilations. Improved CI integration.

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,27 @@ const pkg = require('./package.json');
 const VERSION = pkg.version;
 const IS_PROD = process.env.NODE_ENV === 'production';
 
+const IS_CI = process.env.CI === 'true';
+const CI_COMMIT_ID = IS_CI ? process.env.CI_COMMIT_ID : '';
+const CI_BRANCH = process.env.CI_BRANCH;
+
+const IS_MASTER_BRANCH = IS_CI && CI_BRANCH === 'master';
+
+function filename() {
+  if (IS_CI && !IS_MASTER_BRANCH) {
+    return IS_PROD ? `[name]-dev-${CI_COMMIT_ID}.min.js` : `[name]-dev-${CI_COMMIT_ID}.js`;
+  } else {
+    return IS_PROD ? `[name]-${VERSION}.min.js` : `[name]-${VERSION}.js`;
+  }
+}
+
 module.exports = {
   entry: {
     splitio: './src/core'
   },
 
   output: {
-    filename: IS_PROD ? `[name]-${VERSION}.min.js` : `[name]-${VERSION}.js`,
+    filename: filename(),
     path: __dirname + '/umd',
     library: 'splitio',
     libraryTarget: 'umd'
