a823f9d1bd23bce3bfa0d4b4b67d80dcbd64313a
Facundo Cabrera <cabrerafacundo@gmail.com>
2016-09-14 21:55:43 -0300

    SPLIT-1463 Release 7.0.0-canary.4

--- src/utils/settings/index.js
+++ src/utils/settings/index.js
@@ -89,7 +89,7 @@ const proto = {
   get(name) {
     switch (name) {
       case 'version':
-        return 'javascript-7.0.0-canary.3';
+        return 'javascript-7.0.0-canary.4';
       case 'authorizationKey':
         return this.core.authorizationKey;
       case 'key':
