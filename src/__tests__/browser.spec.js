f018ddea9fb141b831eff85f27ec8995b7a43c6d
Nicolas Zelaya <zelayadn@gmail.com>
2017-04-06 22:25:45 -0300

    Adding Unit Tests

--- src/__tests__/browser.spec.js
+++ src/__tests__/browser.spec.js
@@ -51,11 +51,11 @@ const settingsInLocalStorage = {
   }
 };
 
-function e2eAssetionSuite(config, assert) {
+function e2eAssertionSuite(config, assert) {
   const splitio = SplitFactory(config);
   const client = splitio.client();
 
-  client.ready().then(() => {
+  const getTreatmentTests = () => {
     assert.equal(client.getTreatment('blacklist'), 'not_allowed');
     assert.equal(client.getTreatment('whitelist'), 'allowed');
     assert.equal(client.getTreatment('splitters'), 'on');
@@ -225,10 +225,241 @@ function e2eAssetionSuite(config, assert) {
     assert.equal(client.getTreatment('user_attr_eq_ten', {
       attr: 9
     }), 'off');
+  };
+
+  const getTreatmentsTests = () => {
+    assert.deepEqual(client.getTreatments([
+      // Treatments List
+      'blacklist', 'whitelist', 'splitters', 'qc_team'
+    ]), {
+      // Expected result
+      blacklist: 'not_allowed',
+      whitelist: 'allowed',
+      splitters: 'on',
+      qc_team: 'no'
+    });
+
+    // I'm not sending the attributes
+    assert.deepEqual(client.getTreatments([
+      'employees_between_21_and_50_and_chrome', 'user_attr_gte_10_and_user_attr2_is_not_foo', 'user_account_in_whitelist'
+    ]), {
+      employees_between_21_and_50_and_chrome: 'off',
+      user_attr_gte_10_and_user_attr2_is_not_foo: 'off',
+      user_account_in_whitelist: 'off'
+    });
+
+    assert.deepEqual(client.getTreatments([
+      'employees_between_21_and_50_and_chrome', 'user_attr_gte_10_and_user_attr2_is_not_foo', 'user_account_in_whitelist'
+    ], {
+      // Attributes
+      age: 20,
+      agent: 'foo',
+      attr: 55,
+      attr2: 'bar',
+      account: 'key@split.io'
+    }), {
+      employees_between_21_and_50_and_chrome: 'off',
+      user_attr_gte_10_and_user_attr2_is_not_foo: 'on',
+      user_account_in_whitelist: 'off'
+    });
+
+    assert.deepEqual(client.getTreatments([
+      'employees_between_21_and_50_and_chrome', 'user_attr_gte_10_and_user_attr2_is_not_foo', 'user_account_in_whitelist'
+    ], {
+      // Attributes
+      age: 21,
+      agent: 'chrome',
+      attr: 55,
+      attr2: 'foo',
+      account: 'key_1@split.io'
+    }), {
+      employees_between_21_and_50_and_chrome: 'on',
+      user_attr_gte_10_and_user_attr2_is_not_foo: 'off',
+      user_account_in_whitelist: 'on'
+    });
+
+  };
+
+  client.ready().then(() => {
+    getTreatmentTests();
+    getTreatmentsTests();
 
     assert.end();
   });
+
+    /*
+    assert.equal(client.getTreatment('blacklist'), 'not_allowed');
+    assert.equal(client.getTreatment('whitelist'), 'allowed');
+    assert.equal(client.getTreatment('splitters'), 'on');
+    assert.equal(client.getTreatment('qc_team'), 'no');
+
+    assert.equal(client.getTreatment('employees_between_21_and_50_and_chrome'), 'off');
+    assert.equal(client.getTreatment('employees_between_21_and_50_and_chrome', {
+      age: 21
+    }), 'off');
+    assert.equal(client.getTreatment('employees_between_21_and_50_and_chrome', {
+      age: 20,
+      agent: 'chrome'
+    }), 'off');
+    assert.equal(client.getTreatment('employees_between_21_and_50_and_chrome', {
+      age: 21,
+      agent: 'chrome'
+    }), 'on');
+
+    assert.equal(client.getTreatment('user_attr_gte_10_and_user_attr2_is_not_foo'), 'off');
+    assert.equal(client.getTreatment('user_attr_gte_10_and_user_attr2_is_not_foo', {
+      attr: 55,
+      attr2: 'bar'
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_gte_10_and_user_attr2_is_not_foo', {
+      attr: 55,
+      attr2: 'foo'
+    }), 'off');
+
+    assert.equal(client.getTreatment('user_account_in_whitelist'), 'off');
+    assert.equal(client.getTreatment('user_account_in_whitelist', {
+      account: 'key_1@split.io'
+    }), 'on');
+    assert.equal(client.getTreatment('user_account_in_whitelist', {
+      account: 'key_6@split.io'
+    }), 'off');
+
+    // This is an special case for the browser.
+    assert.equal(client.getTreatment('user_account_in_segment_employees'), 'off');
+    assert.equal(client.getTreatment('user_account_in_segment_employees', {
+      account: 'key_1@split.io'
+    }), 'off');
+
+    assert.equal(client.getTreatment('user_account_in_segment_all'), 'off');
+    assert.equal(client.getTreatment('user_account_in_segment_all', {
+      account: 'something'
+    }), 'on');
+
+    assert.equal(client.getTreatment('user_attr_btw_datetime_1458240947021_and_1458246884077', {
+      attr: new Date('2016-03-17T18:55:47.021Z').getTime()
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_btw_datetime_1458240947021_and_1458246884077', {
+      attr: new Date('2016-03-17T17:55:47.021Z').getTime()
+    }), 'off');
+    assert.equal(client.getTreatment('user_attr_btw_datetime_1458240947021_and_1458246884077', {
+      attr: new Date('2016-03-17T21:34:44.077Z').getTime()
+    }), 'off');
+    assert.equal(client.getTreatment('user_attr_btw_datetime_1458240947021_and_1458246884077'), 'off');
+
+    assert.equal(client.getTreatment('user_attr_btw_number_10_and_20'), 'off');
+    assert.equal(client.getTreatment('user_attr_btw_number_10_and_20', {
+      attr: 9
+    }), 'off');
+    assert.equal(client.getTreatment('user_attr_btw_number_10_and_20', {
+      attr: 21
+    }), 'off');
+    assert.equal(client.getTreatment('user_attr_btw_number_10_and_20', {
+      attr: 15
+    }), 'on');
+
+    assert.equal(client.getTreatment('user_attr_btw_10_and_20'), 'off');
+    assert.equal(client.getTreatment('user_attr_btw_10_and_20', {
+      attr: 9
+    }), 'off');
+    assert.equal(client.getTreatment('user_attr_btw_10_and_20', {
+      attr: 21
+    }), 'off');
+    assert.equal(client.getTreatment('user_attr_btw_10_and_20', {
+      attr: 15
+    }), 'on');
+
+    assert.equal(client.getTreatment('user_attr_lte_datetime_1458240947021', {
+      attr: new Date('2016-03-17T18:55:47.021Z').getTime()
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_lte_datetime_1458240947021', {
+      attr: new Date('2016-03-16T17:55:47.021Z').getTime()
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_lte_datetime_1458240947021', {
+      attr: new Date('2016-03-17T19:55:47.021Z').getTime()
+    }), 'off');
+    assert.equal(client.getTreatment('user_attr_lte_datetime_1458240947021'), 'off');
+
+    assert.equal(client.getTreatment('user_attr_lte_number_10', {
+      attr: 9
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_lte_number_10', {
+      attr: 10
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_lte_number_10', {
+      attr: 11
+    }), 'off');
+    assert.equal(client.getTreatment('user_attr_lte_number_10'), 'off');
+
+    assert.equal(client.getTreatment('user_attr_lte_10', {
+      attr: 9
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_lte_10', {
+      attr: 10
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_lte_10', {
+      attr: 11
+    }), 'off');
+    assert.equal(client.getTreatment('user_attr_lte_10'), 'off');
+
+    assert.equal(client.getTreatment('user_attr_gte_datetime_1458240947021', {
+      attr: new Date('2016-03-17T18:55:47.021Z').getTime()
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_gte_datetime_1458240947021', {
+      attr: new Date('2016-03-17T19:55:47.021Z').getTime()
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_gte_datetime_1458240947021', {
+      attr: new Date('2016-03-17T17:55:47.021Z').getTime()
+    }), 'off');
+    assert.equal(client.getTreatment('user_attr_gte_datetime_1458240947021'), 'off');
+
+    assert.equal(client.getTreatment('user_attr_gte_number_10'), 'off');
+    assert.equal(client.getTreatment('user_attr_gte_number_10', {
+      attr: 10
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_gte_number_10', {
+      attr: 11
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_gte_number_10', {
+      attr: 0
+    }), 'off');
+
+    assert.equal(client.getTreatment('user_attr_gte_10'), 'off');
+    assert.equal(client.getTreatment('user_attr_gte_10', {
+      attr: 10
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_gte_10', {
+      attr: 11
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_gte_10', {
+      attr: 0
+    }), 'off');
+
+    assert.equal(client.getTreatment('user_attr_eq_datetime_1458240947021', {
+      attr: new Date('2016-03-17T00:00:00Z').getTime()
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_eq_datetime_1458240947021', {
+      attr: new Date('2016-03-16T10:01:10Z').getTime()
+    }), 'off');
+    assert.equal(client.getTreatment('user_attr_eq_datetime_1458240947021'), 'off');
+
+    assert.equal(client.getTreatment('user_attr_eq_number_ten'), 'off');
+    assert.equal(client.getTreatment('user_attr_eq_number_ten', {
+      attr: 10
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_eq_number_ten', {
+      attr: 9
+    }), 'off');
+
+    assert.equal(client.getTreatment('user_attr_eq_ten'), 'off');
+    assert.equal(client.getTreatment('user_attr_eq_ten', {
+      attr: 10
+    }), 'on');
+    assert.equal(client.getTreatment('user_attr_eq_ten', {
+      attr: 9
+    }), 'off');
+    */
+
 }
 
-tape('E2E / In Memory', e2eAssetionSuite.bind(null, settingsInMemory));
-tape('E2E / In LocalStorage with In Memory Fallback', e2eAssetionSuite.bind(null, settingsInLocalStorage));
+tape('E2E / In Memory', e2eAssertionSuite.bind(null, settingsInMemory));
+tape('E2E / In LocalStorage with In Memory Fallback', e2eAssertionSuite.bind(null, settingsInLocalStorage));
--- src/__tests__/offline/browser.spec.js
+++ src/__tests__/offline/browser.spec.js
@@ -25,13 +25,31 @@ tape('Browser offline mode', function (assert) {
     assert.equal(await client.getTreatment('testing_split'), 'on');
     assert.equal(await client.getTreatment('testing_split_2'), 'control');
 
+     assert.deepEqual(await client.getTreatments('qa-user', [
+      'testing_split', 'testing_split2'
+      ]), {
+        testing_split: 'on',
+        testing_split_2: 'control'
+      });
+
     factory.settings.features = {
       testing_split: 'on',
-      testing_split_2: 'off'
+      testing_split_2: 'off',
+      testing_split_3: 'custom_treatment'
     };
 
     setTimeout(async function () {
       assert.equal(await client.getTreatment('testing_split_2'), 'off');
+      assert.equal(await client.getTreatment('testing_split_3'), 'custom_treatment');
+
+      assert.deepEqual(await client.getTreatments('qa-user', [
+      'testing_split', 'testing_split2', 'testing_split3', 'testing_not_exist'
+      ]), {
+        testing_split: 'on',
+        testing_split_2: 'off',
+        testing_split_3: 'custom_treatment',
+        testing_not_exist: 'control'
+      });
 
       client.destroy();
       assert.end();
