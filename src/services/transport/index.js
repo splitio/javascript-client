8c27848d3517accdb96d80cabcf319581b8c2bc0
Nicolas Zelaya <zelayadn@gmail.com>
2017-05-03 18:14:48 -0300

    Fetcher - Changing statusText comparison for resp.ok flag

--- src/services/transport/index.js
+++ src/services/transport/index.js
@@ -21,7 +21,7 @@ const log = require('debug')('splitio-services:service');
 
 function Fetcher(request) {
   return fetch(request).then(resp => {
-    if (resp.statusText === 'OK') {
+    if (resp.ok) {
       return resp;
     } else {
       log('throw error because status text is not OK');
