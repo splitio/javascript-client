2fcd1381de8d0fea3681fda53b35ffa789b5e298
Nicolas Zelaya <zelayadn@gmail.com>
2017-08-10 15:42:37 -0300

    Changing timeTracker start with promise test

--- src/utils/__tests__/timeTracker/index.spec.js
+++ src/utils/__tests__/timeTracker/index.spec.js
@@ -41,8 +41,9 @@ tape('TIME TRACKER / should have the correct API', assert => {
 
 tape('TIME TRACKER start() / should return the correct type', assert => {
   const promise = new Promise(res => {
-    setTimeout(res, 500);
+    setTimeout(res, 1000);
   });
+  promise.crap = true;
   const startNormal = tracker.start(tracker.TaskNames.SDK_READY);
   const startNormalFake = tracker.start('fakeTask3');
   const startWithPromise = tracker.start('fakeTask4', false, promise);
@@ -50,7 +51,7 @@ tape('TIME TRACKER start() / should return the correct type', assert => {
   assert.equal(typeof startNormal, 'function', 'If we call start without a promise, it will return the stop function,');
   assert.equal(typeof startNormal.setCollectorForTask, 'function', 'that has a function as well for setting the collector at a defered time, because it has a registered cb but no collector received.');
   assert.equal(typeof startNormalFake.setCollectorForTask, 'undefined', 'If no callback is registered for the task, no collectors setup function is attached to returned one.');
-  assert.deepEqual(startWithPromise, promise, 'But if pass a promise, we will get that promise back, with the callbacks attached.');
+  assert.equal(typeof startWithPromise.then, 'function', 'But if we pass a promise, we will get a promise back, with the necessary callbacks already handled.');
 
   assert.end();
 });
