a73c0edd619f2482d8ba158ce0e4f532a96989e3
Nicolas Zelaya <zelayadn@gmail.com>
2017-10-05 18:52:46 -0300

    Making key setting optional for localhost mode.

--- src/client/browser.js
+++ src/client/browser.js
@@ -9,11 +9,16 @@ const keyParser = require('../utils/key/parser');
 
 function FixKey(context): SplitClient {
   const settings = context.get(context.constants.SETTINGS);
-  // In the browser land, the key is required
-  keyParser(get(settings, 'core.key', undefined));
+  let key = get(settings, 'core.key', undefined);
+
+  if (settings.mode === 'localhost' && key === undefined) {
+    settings.core.key = 'localhost_key';
+  } else {
+    keyParser(key);
+  }
 
   const client = ClientFactory(context);
-
+  // In the browser land, the key is required
   client.isBrowserClient = true;
   client.getTreatment = client.getTreatment.bind(client, settings.core.key);
   client.getTreatments = client.getTreatments.bind(client, settings.core.key);
