05a5d2e05f28130ae130e4fb7a5b7708363b8cf2
Nicolas Zelaya <zelayadn@gmail.com>
2017-11-10 18:36:36 -0300

    Updating Node & Browser default settings with new configurations for events

--- src/utils/settings/defaults/browser.js
+++ src/utils/settings/defaults/browser.js
@@ -7,6 +7,8 @@ module.exports = {
     // how many quick retries we will do while starting up the SDK.
     retriesOnFailureBeforeReady: 1,
     // maximun amount of time used before notifies me a timeout.
-    readyTimeout: 1.5
+    readyTimeout: 1.5,
+    // Amount of time we will wait before the first push of events.
+    eventsFirstPushWindow: 10
   }
 };
