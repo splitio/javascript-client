6ce13051b43879d886bf0038e1833ddcbd88d257
Nicolas Zelaya <zelayadn@gmail.com>
2019-01-17 16:49:54 -0300

    Updating NodeJS defaults

--- src/utils/settings/defaults/node.js
+++ src/utils/settings/defaults/node.js
@@ -1,11 +1,11 @@
 export default {
   startup: {
-    // stress the request time used while starting up the SDK.
+    // Stress the request time used while starting up the SDK.
     requestTimeoutBeforeReady: 15,
-    // how many quick retries we will do while starting up the SDK.
-    retriesOnFailureBeforeReady: 0,
-    // maximun amount of time used before notifies me a timeout.
-    readyTimeout: 0,
+    // How many quick retries we will do while starting up the SDK.
+    retriesOnFailureBeforeReady: 1,
+    // Maximum amount of time used before notifies me a timeout.
+    readyTimeout: 15,
     // Don't wait a specific time for first flush on Node, no page load here.
     eventsFirstPushWindow: 0
   }
