118abbbe688fa377c3317d9b81173d773179454b
Nicolas Zelaya <zelayadn@gmail.com>
2019-01-30 18:00:03 -0300

    Api Key validator

---
+++ src/utils/inputValidation/apiKey.js
@@ -0,0 +1,19 @@
+import { isString } from '../lang';
+import logFactory from '../logger';
+const log = logFactory('', {
+  displayAllErrors: true
+});
+
+export default function validateApiKey(maybeApiKey) {
+  if (maybeApiKey == undefined) { // eslint-disable-line eqeqeq
+    log.error('Factory instantiation: you passed a null or undefined api_key, api_key must be a non-empty string.');
+  } else if (isString(maybeApiKey)) {
+    if (maybeApiKey.length > 0) return maybeApiKey;
+
+    log.error('Factory instantiation: you passed an empty api_key, api_key must be a non-empty string.');
+  } else {
+    log.error('Factory instantiation: you passed an invalid api_key, api_key must be a non-empty string.');
+  }
+
+  return false;
+}
