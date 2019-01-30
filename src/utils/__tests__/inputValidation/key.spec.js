b3d2f7b5530254f7c116cd6d08cdef5b60b182ae
Nicolas Zelaya <zelayadn@gmail.com>
2019-01-30 13:43:42 -0300

    Key input validation test

--- src/utils/__tests__/inputValidation/key.spec.js
+++ src/utils/__tests__/inputValidation/key.spec.js
@@ -44,81 +44,157 @@ const invalidKeyObjects = [
   { random: 'asd' }
 ];
 
-tape('INPUT VALIDATION - String key / Should return false and log error if key is invalid', assert => {
-  const consoleMethod = !isNode ? 'error' : 'log';
+tape('INPUT VALIDATION for Key', t => {
+  t.test('String and Object keys / Should return the key with no errors logged if the key is correct', assert => {
+    const consoleMethod = !isNode ? 'error' : 'log';
+    const validKey = 'validKey';
+    const validObjKey = {
+      matchingKey: 'valid', bucketingKey: 'alsoValid'
+    };
+    // Spy on the console method that will be used.
+    console[consoleMethod] && sinon.spy(console, consoleMethod);
 
-  // Spy on the console method that will be used.
-  console[consoleMethod] && sinon.spy(console, consoleMethod);
+    assert.deepEqual(validateKey(validKey, 'some_method_keys'), validKey, 'It will return the valid key.');
+    assert.notOk(console[consoleMethod].calledWithMatch('[ERROR] some_method_keys'), 'No errors should be logged.');
 
-  for (let i = 0; i < invalidKeys.length; i++) {
-    const invalidKey = invalidKeys[i]['key'];
-    const expectedLog = invalidKeys[i]['msg']('key');
+    assert.deepEqual(validateKey(validObjKey, 'some_method_keys'), validObjKey, 'It will return the valid key.');
+    assert.notOk(console[consoleMethod].calledWithMatch('[ERROR] some_method_keys'), 'No errors should be logged.');
+    // Restore the spy.
+    console[consoleMethod].restore();
+
+    assert.end();
+  });
+
+  t.test('String key / Should return false and log error if key is invalid', assert => {
+    const consoleMethod = !isNode ? 'error' : 'log';
+    console[consoleMethod] && sinon.spy(console, consoleMethod);
+
+    for (let i = 0; i < invalidKeys.length; i++) {
+      const invalidKey = invalidKeys[i]['key'];
+      const expectedLog = invalidKeys[i]['msg']('key');
+
+      assert.equal(validateKey(invalidKey, 'test_method'), false, 'Invalid keys should return false.');
+      assert.ok(console[consoleMethod].calledWithMatch(`[ERROR] test_method: ${expectedLog}`), 'The error should be logged for the invalid key.');
+
+      console[consoleMethod].resetHistory();
+    }
+    console[consoleMethod].restore();
+
+    assert.end();
+  });
+
+  t.test('String key / Should return stringified version of the key if it is convertible to one and log a warning.', assert => {
+    // Spy on the console log method
+    sinon.spy(console, 'log');
+
+    for (let i = 0; i < stringifyableKeys.length; i++) {
+      const invalidKey = stringifyableKeys[i]['key'];
+      const expectedLog = stringifyableKeys[i]['msg']('key', invalidKey);
+
+      assert.equal(validateKey(invalidKey, 'test_method'), String(invalidKey), 'Stringifyable keys should be transformed to string and returned.');
+      assert.notOk(console.log.calledWithMatch(`[WARN] test_method: ${expectedLog}`), 'The warning should not be logged by default for the invalid key.');
+
+      // Allow for warnings to be shown.
+      LoggerAPI.setLogLevel(LoggerAPI.LogLevel.WARN);
+
+      validateKey(invalidKey, 'test_method');
+      assert.ok(console.log.calledWithMatch(`[WARN]  test_method: ${expectedLog}`), 'But if the logger allows for warnings, it should be logged.');
+
+      // Disable warnings again.
+      LoggerAPI.disable();
+
+      console.log.resetHistory();
+    }
+
+    // Restore spy.
+    console.log.restore();
+
+    assert.end();
+  });
+
+  t.test('Object key / Should return false and log error if a part of the key is invalid', assert => {
+    const consoleMethod = !isNode ? 'error' : 'log';
+
+    // Spy on the console method that will be used.
+    sinon.spy(console, consoleMethod);
+    // Test invalid object format
+    for (let i = 0; i < invalidKeyObjects.length; i++) {
+      assert.equal(validateKey(invalidKeyObjects[i], 'test_method'), false, 'Invalid key objects should return false.');
+      assert.ok(console[consoleMethod].calledWithMatch(`[ERROR] test_method: ${errorMsgs.WRONG_KEY_PROPS}`), 'The error should be logged for the invalid key.');
+
+      console[consoleMethod].resetHistory();
+    }
+    // Test invalid matchingKey
+    for (let i = 0; i < invalidKeys.length; i++) {
+      const invalidKey = {
+        matchingKey: invalidKeys[i]['key'],
+        bucketingKey: 'thisIsValid'
+      };
+      const expectedLog = invalidKeys[i]['msg']('matchingKey');
+
+      assert.equal(validateKey(invalidKey, 'test_method'), false, 'Invalid keys should return false.');
+      assert.ok(console[consoleMethod].calledWithMatch(`[ERROR] test_method: ${expectedLog}`), 'The error should be logged for the invalid key.');
+
+      console[consoleMethod].resetHistory();
+    }
+    // Test invalid bucketingKey
+    for (let i = 0; i < invalidKeys.length; i++) {
+      const invalidKey = {
+        matchingKey: 'thisIsValidToo',
+        bucketingKey: invalidKeys[i]['key']
+      };
+      const expectedLog = invalidKeys[i]['msg']('bucketingKey');
+
+      assert.equal(validateKey(invalidKey, 'test_method'), false, 'Invalid keys should return false.');
+      assert.ok(console[consoleMethod].calledWithMatch(`[ERROR] test_method: ${expectedLog}`), 'The error should be logged for the invalid key.');
+
+      console[consoleMethod].resetHistory();
+    }
+    // Just one test that if both are invalid we get the log for both.
+    let invalidKey = {
+      matchingKey:  invalidKeys[0]['key'],
+      bucketingKey: invalidKeys[1]['key']
+    };
+    let expectedLogMK = invalidKeys[0]['msg']('matchingKey');
+    let expectedLogBK = invalidKeys[1]['msg']('bucketingKey');
 
     assert.equal(validateKey(invalidKey, 'test_method'), false, 'Invalid keys should return false.');
-    assert.ok(console.log.calledWithMatch(`[ERROR] test_method: ${expectedLog}`), 'The error should be logged for the invalid key.');
+    assert.ok(console[consoleMethod].calledWithMatch(`[ERROR] test_method: ${expectedLogMK}`), 'The error should be logged for the invalid key property.');
+    assert.ok(console[consoleMethod].calledWithMatch(`[ERROR] test_method: ${expectedLogBK}`), 'The error should be logged for the invalid key property.');
+    console[consoleMethod].resetHistory();
 
-    console[consoleMethod].reset();
-  }
-  // Restore spy.
-  console[consoleMethod].restore();
+    // Restore spy.
+    console[consoleMethod].restore();
 
-  assert.end();
-});
+    assert.end();
+  });
 
-tape('INPUT VALIDATION - String key / Should return stringified version of the key if it is convertible to one and log a warning.', assert => {
-  // Spy on the console log method
-  sinon.spy(console, 'log');
+  t.test('Object key / Should return stringified version of the key props if those are convertible and log the corresponding warnings', assert => {
+    // Spy on the console log method
+    sinon.spy(console, 'log');
 
-  for (let i = 0; i < stringifyableKeys.length; i++) {
-    const invalidKey = stringifyableKeys[i]['key'];
-    const expectedLog = stringifyableKeys[i]['msg']('key', invalidKey);
+    let invalidKey = {
+      matchingKey:  stringifyableKeys[0]['key'],
+      bucketingKey: stringifyableKeys[1]['key']
+    };
+    let expectedKey = {
+      matchingKey: String(invalidKey.matchingKey),
+      bucketingKey: String(invalidKey.bucketingKey),
+    };
+    let expectedLogMK = stringifyableKeys[0]['msg']('matchingKey', invalidKey.matchingKey);
+    let expectedLogBK = stringifyableKeys[1]['msg']('bucketingKey', invalidKey.bucketingKey);
 
-    assert.equal(validateKey(invalidKey, 'test_method'), String(invalidKey), 'Stringifyable keys should be transformed to string and returned.');
-    assert.notOk(console.log.calledWithMatch(`[WARN] test_method: ${expectedLog}`), 'The warning should not be logged by default for the invalid key.');
-
-    // Allow for warnings to be shown.
     LoggerAPI.setLogLevel(LoggerAPI.LogLevel.WARN);
 
-    validateKey(invalidKey, 'test_method');
-    assert.ok(console.log.calledWithMatch(`[WARN]  test_method: ${expectedLog}`), 'But if the logger allows for warnings, it should be logged.');
+    assert.deepEqual(validateKey(invalidKey, 'test_method'), expectedKey, 'If a key object had stringifyable values, those will be stringified and Key returned.');
+    assert.ok(console.log.calledWithMatch(`[WARN]  test_method: ${expectedLogMK}`), 'The warning should be logged for the stringified prop if warnings are enabled.');
+    assert.ok(console.log.calledWithMatch(`[WARN]  test_method: ${expectedLogBK}`), 'The warning should be logged for the stringified prop if warnings are enabled.');
+    console.log.resetHistory();
 
-    // Allow for warnings to be shown.
     LoggerAPI.disable();
+    // Restore spy.
+    console.log.restore();
 
-    console.log.reset();
-  }
-
-  // Restore spy.
-  console.log.restore();
-
-  assert.end();
-});
-
-tape('INPUT VALIDATION - Object key / Should return false and log error if key is invalid', assert => {
-  const consoleMethod = !isNode ? 'error' : 'log';
-
-  // Spy on the console method that will be used.
-  sinon.spy(console, consoleMethod);
-
-  for (let i = 0; i < invalidKeyObjects.length; i++) {
-    assert.equal(validateKey(invalidKeyObjects[i], 'test_method'), false, 'Invalid keys should return false.');
-    assert.ok(console.log.calledWithMatch(`[ERROR] test_method: ${errorMsgs.WRONG_KEY_PROPS}`), 'The error should be logged for the invalid key.');
-
-    console[consoleMethod].reset();
-  }
-
-  for (let i = 0; i < invalidKeys.length; i++) {
-    const invalidKey = invalidKeys[i]['key'];
-    const expectedLog = invalidKeys[i]['msg']('key');
-
-    assert.equal(validateKey(invalidKey, 'test_method'), false, 'Invalid keys should return false.');
-    assert.ok(console.log.calledWithMatch(`[ERROR] test_method: ${expectedLog}`), 'The error should be logged for the invalid key.');
-
-    console[consoleMethod].reset();
-  }
-
-  // Restore spy.
-  console[consoleMethod].restore();
-
-  assert.end();
+    assert.end();
+  });
 });
