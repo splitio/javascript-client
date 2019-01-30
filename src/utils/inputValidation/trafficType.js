fc5d35897b1dc9b53beff5c776a7bebbc67b2999
Nicolas Zelaya <zelayadn@gmail.com>
2019-01-30 13:41:06 -0300

    trafficType validator

---
+++ src/utils/inputValidation/trafficType.js
@@ -0,0 +1,33 @@
+import { isString } from '../lang';
+import logFactory from '../logger';
+const log = logFactory('', {
+  displayAllErrors: true
+});
+
+const CAPITAL_LETTERS_REGEX = /[A-Z]/;
+
+export default function validateTrafficType(maybeTT, method) {
+  if (maybeTT == undefined) { // eslint-disable-line eqeqeq
+    log.error(`${method}: you passed a null or undefined traffic_type_name, traffic_type_name must be a non-empty string.`);
+  } else if (!isString(maybeTT)) {
+    log.error(`${method}: you passed an invalid traffic_type_name, traffic_type_name must be a non-empty string.`);
+  } else {
+    if (maybeTT.length === 0) {
+      log.error(`${method}: you passed an empty traffic_type_name, traffic_type_name must be a non-empty string.`);
+    } else {
+      if (CAPITAL_LETTERS_REGEX.test(maybeTT)) {
+        log.warn(`${method}: traffic_type_name should be all lowercase - converting string to lowercase.`);
+        maybeTT = maybeTT.toLowerCase();
+      }
+
+      return maybeTT;
+    }
+  }
+
+  return false;
+}
+
+
+
+
+
