4d76a4b59e293c89cd1d6866734238a6395d576c
Nicolas Zelaya <zelayadn@gmail.com>
2019-03-28 09:49:32 -0300

    Adding internal metrics tests for node

--- src/__tests__/node.spec.js
+++ src/__tests__/node.spec.js
@@ -6,15 +6,13 @@ import SettingsFactory from '../utils/settings';
 import evaluationsSuite from './nodeSuites/evaluations.spec';
 import eventsSuite from './nodeSuites/events.spec';
 import impressionsSuite from './nodeSuites/impressions.spec';
+import metricsSuite from './nodeSuites/metrics.spec';
 import impressionsListenerSuite from './nodeSuites/impressions-listener.spec';
 import expectedTreatmentsSuite from './nodeSuites/expected-treatments.spec';
 import managerSuite from './nodeSuites/manager.spec';
 
 import splitChangesMock1 from './mocks/splitchanges.since.-1.json';
 import splitChangesMock2 from './mocks/splitchanges.since.1457552620999.json';
-import mySegmentsFacundo from './mocks/mysegments.facundo@split.io.json';
-import mySegmentsNicolas from './mocks/mysegments.nicolas@split.io.json';
-import mySegmentsMarcio from './mocks/mysegments.marcio@split.io.json';
 
 const settings = SettingsFactory({
   core: {
@@ -41,9 +39,6 @@ const mock = new MockAdapter(axios);
 
 mock.onGet(settings.url('/splitChanges?since=-1')).reply(200, splitChangesMock1);
 mock.onGet(settings.url('/splitChanges?since=1457552620999')).reply(200, splitChangesMock2);
-mock.onGet(settings.url('/mySegments/facundo@split.io')).reply(200, mySegmentsFacundo);
-mock.onGet(settings.url('/mySegments/nicolas@split.io')).reply(200, mySegmentsNicolas);
-mock.onGet(settings.url('/mySegments/marcio@split.io')).reply(200, mySegmentsMarcio);
 
 tape('## Node JS - E2E CI Tests ##', async function (assert) {
   /* Check client evaluations. */
@@ -52,6 +47,9 @@ tape('## Node JS - E2E CI Tests ##', async function (assert) {
   /* Check impressions */
   assert.test('E2E / Impressions', impressionsSuite.bind(null, key, mock));
   assert.test('E2E / Impressions listener', impressionsListenerSuite);
+
+  /* Check metrics */
+  assert.test('E2E / Metrics', metricsSuite.bind(null, key, mock));
 
   /* Check events in memory */
   assert.test('E2E / Events', eventsSuite.bind(null, mock));
