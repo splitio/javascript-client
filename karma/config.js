54c2d9225ea2556498406de4db5ebddfb87012c6
NicoZelaya <zelayadn@gmail.com>
2019-09-16 11:58:33 -0300

    Removing destroy suite from base karma conf

--- karma/config.js
+++ karma/config.js
@@ -22,7 +22,6 @@ module.exports = {
   // list of files / patterns to load in the browser
   files: [
     '*/__tests__/**/*.spec.js',
-    '__tests__/destroy/browser.spec.js',
     {
       pattern: 'engine/__tests__/engine/mocks/murmur3*.csv',
       watched: false,
@@ -49,7 +48,6 @@ module.exports = {
   // prepare code for the browser using webpack
   preprocessors: {
     '*/__tests__/**/*.spec.js': ['webpack'],
-    '__tests__/destroy/browser.spec.js': ['webpack']
   },
 
   webpack: {
