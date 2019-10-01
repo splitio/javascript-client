1cb105bc29251923f16af23dba0c89b5a56696b9
NicoZelaya <zelayadn@gmail.com>
2019-10-01 01:22:55 -0300

    Updating webpack for UMD builds

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+module.exports = {
+  entry: {
+    split: ['./src/umd']
+  },
+
+  output: {
+    path: __dirname + '/umd',
+    library: 'splitio',
+    libraryTarget: 'umd'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+
+  node: {
+    fs: 'empty',
+    module: 'empty',
+    console: false
+  }
+};
