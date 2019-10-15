3ed918d0042ff1a9a2e31863fe108aaab221bf8f
NicoZelaya <zelayadn@gmail.com>
2019-10-15 14:16:57 -0700

    Removing old babelrc backup. Linting fixes on babel.config

--- babel.config.js
+++ babel.config.js
@@ -10,33 +10,33 @@ module.exports = function (api) {
     case 'development':
     case 'production':
       presets.push(
-        ["@babel/preset-env", {
-          "modules": "commonjs"
+        ['@babel/preset-env', {
+          'modules': 'commonjs'
         }]
       );
       break;
 
     case 'test':
-      presets.push(["@babel/preset-env", {
-        "useBuiltIns": "usage",
-        "modules": "commonjs",
-        "corejs": "3",
-        "targets": {
-          "ie": "10",
-          "node": "4"
+      presets.push(['@babel/preset-env', {
+        'useBuiltIns': 'usage',
+        'modules': 'commonjs',
+        'corejs': '3',
+        'targets': {
+          'ie': '10',
+          'node': '4'
         }
       }]);
-      plugins.push("@babel/transform-runtime");
+      plugins.push('@babel/transform-runtime');
       break;
 
     default: // es6 build
-      presets.push(["@babel/preset-env", {
-        "modules": false
+      presets.push(['@babel/preset-env', {
+        'modules': false
       }]);
       plugins.push(
-        "@babel/plugin-proposal-object-rest-spread",
-        ["@babel/transform-runtime", {
-          "useESModules": true
+        '@babel/plugin-proposal-object-rest-spread',
+        ['@babel/transform-runtime', {
+          'useESModules': true
         }]
       );
       break;
