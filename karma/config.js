a603111cab02dfdcee1bf1b84b70771ce676ddbb
NicoZelaya <zelayadn@gmail.com>
2019-10-16 15:33:59 -0700

    Karma conf fix

--- karma/config.js
+++ karma/config.js
@@ -57,11 +57,6 @@ module.exports = {
           test: /\.js$/,
           exclude: /node_modules/,
           loader: 'babel-loader'
-        },
-        {
-          test: /\.json$/,
-          exclude: /node_modules/,
-          loader: 'json-loader'
         }
       ]
     },
