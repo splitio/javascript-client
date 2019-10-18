b2cd03e3e0f81194c71fcf29389208e185b2ef11
NicoZelaya <zelayadn@gmail.com>
2019-10-17 18:00:45 -0700

    adding -dev to developpment bundle

--- webpack.ci.prod.js
+++ webpack.ci.prod.js
@@ -7,6 +7,6 @@ const VERSION = pkg.version;
 module.exports = env => merge(common, {
   mode: 'production',
   output: {
-    filename: `[name]${env.branch !== 'master' ? `-${env.commit_hash}` : `-${VERSION}`}.min.js`
+    filename: `[name]${env.branch !== 'master' ? `-dev-${env.commit_hash}` : `-${VERSION}`}.min.js`
   }
 });
