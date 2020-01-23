3c6b7049b0f93abe96958f4b69392a1e5bf19c45
NicoZelaya <zelayadn@gmail.com>
2020-01-23 04:44:42 -0300

    calculate update on localhost browser

--- src/services/splitChanges/offline/browser.js
+++ src/services/splitChanges/offline/browser.js
@@ -1,8 +1,32 @@
 import { isObject, forOwn } from '../../../utils/lang';
 import parseCondition from './parseCondition';
 
+let previousMock = { '':1 };
+
+function mockUpdated(currentData) {
+  const names = Object.keys(currentData);
+
+  // Different amount of items
+  if (names.length !== Object.keys(previousMock).length) {
+    previousMock = currentData;
+    return true;
+  }
+
+  return names.some(name => {
+    const newSplit = !previousMock[name];
+    const newTreatment = previousMock[name] !== currentData[name];
+
+    previousMock = currentData;
+    
+    return newSplit || newTreatment;
+  });
+}
+
 function getConfigurationFromSettings(settings) {
   const mockSettings = settings.features || {};
+
+  if (!mockUpdated(mockSettings)) return false;
+
   const splitObjects = {};
 
   forOwn(mockSettings, (data, splitName) => {
