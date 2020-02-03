e5576bbd2b5d04f3c23e0fae1b01ff8c3d5e2f19
Emiliano Sanchez <emiliano.sanchez@split.io>
2020-02-03 17:35:48 -0300

    refactor: moved SplitTracker functions into SplitTrackerManager class

--- src/integrations/browser.js
+++ src/integrations/browser.js
@@ -1,29 +1,24 @@
-import { providePlugin, SplitTracker, sdkOptions } from './ga/splitTracker';
+import GaSplitTrackerManager from './ga/splitTracker';
 import { isObject } from '../utils/lang';
-import logFactory from '../utils/logger';
-const log = logFactory('splitio');
 
 const browserSetupIntegrations = (context) => {
   const settings = context.get(context.constants.SETTINGS);
-  const storage = context.get(context.constants.STORAGE);
 
   if (settings.integrations) {
     if (settings.integrations.ga2split) {
-      sdkOptions.eventHandler = function (event) {
-        storage.events.track(event);
-      };
-      if (settings.core.trafficType)
-        sdkOptions.identities = [{ key: settings.core.key, trafficType: settings.core.trafficType }];
 
-      if (isObject(settings.integrations.ga2split))
-        Object.assign(sdkOptions, settings.integrations.ga2split);
+      const storage = context.get(context.constants.STORAGE);
+      const ga2split = settings.integrations.ga2split;
 
-      // @TODO review error condition and message
-      if (!sdkOptions.identities || sdkOptions.identities.length === 0) {
-        log.error('A traffic type is required for tracking GA hits as Split events');
-      }
-      // Register the plugin, even if config is invalid, since, if not provided, it will block `ga` command queue.
-      providePlugin('splitTracker', SplitTracker);
+      const gaSdkOptions = Object.assign(
+        {
+          eventHandler: function (event) {
+            storage.events.track(event);
+          },
+          identities: [{ key: settings.core.key, trafficType: settings.core.trafficType }]
+        },
+        isObject(ga2split) ? ga2split : {});
+      new GaSplitTrackerManager(gaSdkOptions);
     }
   }
 };
--- src/integrations/ga/splitTracker.js
+++ src/integrations/ga/splitTracker.js
@@ -1,146 +1,118 @@
-// CHECK provide.js at autotrack
-export function providePlugin(pluginName, pluginConstructor) {
-  var ga = window[window['GoogleAnalyticsObject'] || 'ga'];
-  if (typeof ga == 'function') {
-    ga('provide', pluginName, pluginConstructor);
-  }
-}
 
-function defaultHitFilter() {
-  return true;
-}
+import logFactory from '../../utils/logger';
+const log = logFactory('splitio');
 
-function defaultHitMapper(options) {
-  var defaultOptions = {
-    key: undefined,
-    fieldKey: 'clientId',
-    trafficTypeName: 'user',
-    eventTypeIdPrefix: {
-      pageview: 'ga-pageview',
-      screenview: 'ga-screenview',
-      event: 'ga-event',
-      social: 'ga-social',
-      timing: 'ga-timing',
-    },
-    eventTypeId: {
-      pageview: 'page',
-      screenview: 'screenName',
-      event: 'eventAction',
-      social: 'socialAction',
-      timing: 'timingVar',
-    },
-    eventValue: {
-      event: 'eventValue',
-      timing: 'timingValue',
-    },
-    eventProperties: {
-      event: ['eventCategory', 'eventLabel'],
-      social: ['socialNetwork', 'socialTarget'],
-      timing: ['timingCategory', 'timingLabel'],
+export default class SplitTrackerManager {
+
+  // @TODO check provide.js at autotrack
+  static providePlugin(pluginName, pluginConstructor) {
+    var ga = window[window['GoogleAnalyticsObject'] || 'ga'];
+    if (typeof ga == 'function') {
+      ga('provide', pluginName, pluginConstructor);
     }
-  };
-  const opts = Object.assign({}, defaultOptions, options);
+  }
 
-  return function (model) {
+  static defaultHitFilter() {
+    return true;
+  }
 
-    var hitType = model.get('hitType');
-    var eventTypeId =
-      (opts.eventTypeIdPrefix[hitType] || 'ga') + '.' +
-      (model.get(opts.eventTypeId[hitType]) || '');
-    var value = model.get(opts.eventValue[hitType]);
-    var properties = {};
-    var fields = opts.eventProperties[hitType];
-    if (fields) {
-      var length = fields.length;
-      for (var i = 0; i < length; i++) {
-        properties[fields[i]] = model.get(fields[i]);
+  static defaultHitMapper(options) {
+    var defaultOptions = {
+      eventTypeIdPrefix: {
+        pageview: 'ga-pageview',
+        screenview: 'ga-screenview',
+        event: 'ga-event',
+        social: 'ga-social',
+        timing: 'ga-timing',
+      },
+      eventTypeId: {
+        pageview: 'page',
+        screenview: 'screenName',
+        event: 'eventAction',
+        social: 'socialAction',
+        timing: 'timingVar',
+      },
+      eventValue: {
+        event: 'eventValue',
+        timing: 'timingValue',
+      },
+      eventProperties: {
+        event: ['eventCategory', 'eventLabel'],
+        social: ['socialNetwork', 'socialTarget'],
+        timing: ['timingCategory', 'timingLabel'],
       }
-    }
-
-    return {
-      eventTypeId: eventTypeId,
-      trafficTypeName: opts.trafficTypeName,
-      value: value,
-      timestamp: Date.now(),
-      key: model.get(opts.fieldKey),
-      properties: properties
     };
-  };
-}
+    const opts = Object.assign({}, defaultOptions, options);
 
-// Post data to our BE with beacon API. If not Beacon API available, fallback to XHR.
-function beaconPost(url, payload) {
-  return navigator.sendBeacon ? navigator.sendBeacon(url, payload) : xhrPost(url, payload);
-}
+    return function (model) {
 
-// XHR request fallback.
-function xhrPost(url, payload) {
-  try {
-    var req = new XMLHttpRequest();
-    req.open('POST', url, true);
-    req.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
-    req.send(payload);
-  } catch (e) {
-    // noop
-  }
-
-  return true;
-}
-
-// `sendEvent` returns a eventHandler that perform beacon/xhr requests to send events, given an `authorizationKey`, and optional `sdk` name and `eventsUrl` URL
-// This is usefull to use the plugin standalone.
-export function sendEvent(authorizationKey, sdk = 'ga', eventsUrl = 'https://events.split.io/api/events/beacon') {
-  // There's nothing to fallback for. Thus, we return a noop function as eventHandler
-  if (!(navigator && navigator.sendBeacon) && (!XMLHttpRequest || typeof XDomainRequest !== 'undefined'))
-    return function () { };
-
-  return function (event, model) {
-    const payload = JSON.stringify({
-      entries: [event],
-      token: authorizationKey,
-      sdk: sdk,
-    });
-    return model.get('transport') === 'beacon' ? beaconPost(eventsUrl, payload) : xhrPost(eventsUrl, payload);
-  };
-}
-
-const defaultOptions = {
-  hitFilter: defaultHitFilter,
-  hitMapper: defaultHitMapper(),
-  identities: [],
-};
-
-export const sdkOptions = {};
-
-/**
- * Constructor for the SplitTracker plugin.
- */
-export function SplitTracker(tracker, options) {
-
-  const opts = Object.assign({}, defaultOptions, options, sdkOptions);
-
-  this.tracker = tracker;
-
-  // Overwrite sendHitTask to perform plugin tasks:
-  // 1) filter hits
-  // 2) map hits to Splitevents
-  // 3) handle events, i.e., send them to Split BE
-  const originalSendHitTask = this.tracker.get('sendHitTask');
-  this.tracker.set('sendHitTask', function (model) {
-    originalSendHitTask(model);
-    if (opts.hitFilter(model)) {
-      const eventData = opts.hitMapper(model);
-      if (opts.eventHandler) {
-        for (let i = 0; i < opts.identities.length; i++) {
-          const identity = opts.identities[i];
-          const event = Object.assign({
-            key: identity.key,
-            trafficTypeName: identity.trafficType,
-            timestamp: Date.now(),
-          }, eventData);
-          opts.eventHandler(event, model);
+      var hitType = model.get('hitType');
+      var eventTypeId =
+        (opts.eventTypeIdPrefix[hitType] || 'ga') + '.' +
+        (model.get(opts.eventTypeId[hitType]) || '');
+      var value = model.get(opts.eventValue[hitType]);
+      var properties = {};
+      var fields = opts.eventProperties[hitType];
+      if (fields) {
+        var length = fields.length;
+        for (var i = 0; i < length; i++) {
+          properties[fields[i]] = model.get(fields[i]);
         }
       }
+
+      return {
+        eventTypeId: eventTypeId,
+        value: value,
+        properties: properties
+      };
+    };
+  }
+
+  constructor(sdkOptions) {
+    // Constructor for the SplitTracker plugin.
+
+    function SplitTracker(tracker, pluginOptions) {
+
+      const opts = Object.assign({
+        hitFilter: SplitTrackerManager.defaultHitFilter,
+        hitMapper: SplitTrackerManager.defaultHitMapper(),
+      }, pluginOptions, sdkOptions);
+
+      this.tracker = tracker;
+
+      // @TODO review error condition and message
+      if (!opts.identities || sdkOptions.identities.length === 0) {
+        log.error('A traffic type is required for tracking GA hits as Split events');
+      }      
+
+      // Overwrite sendHitTask to perform plugin tasks:
+      // 1) filter hits
+      // 2) map hits to Splitevents
+      // 3) handle events, i.e., send them to Split BE
+      const originalSendHitTask = tracker.get('sendHitTask');
+      tracker.set('sendHitTask', function (model) {
+        originalSendHitTask(model);
+        if (opts.hitFilter(model)) {
+          const eventData = opts.hitMapper(model);
+          if (opts.eventHandler) {
+            const timestamp = Date.now();
+            for (let i = 0; i < opts.identities.length; i++) {
+              const identity = opts.identities[i];
+              const event = Object.assign({
+                key: identity.key,
+                trafficTypeName: identity.trafficType,
+                timestamp,
+              }, eventData);
+              opts.eventHandler(event, model);
+            }
+          }
+        }
+      });
     }
-  });
+
+    // Register the plugin, even if config is invalid, since, if not provided, it will block `ga` command queue.
+    SplitTrackerManager.providePlugin('splitTracker', SplitTracker);
+
+  }
+
 }
