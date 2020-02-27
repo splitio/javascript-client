218788c15c4455a04b0bfc111cd1dbcbbda1703b
Rohan Murthy <rohanmur@hotmail.com>
2020-02-27 14:27:38 -0800

    add comment

--- src-new-package/index.js
+++ src-new-package/index.js
@@ -3,4 +3,6 @@ import SplitCacheLocalStorage from './storage/SplitCache'
 import KeyBuilderLocalStorage from './storage/KeysLocalStorage';
 import SettingsFactory from './utils/settings';
 
+// ClientFactory exported in order to call getTreatmentsWithConfig, we will pass in a `storage` object to this function
+// SplitCacheLocalStorage, KeyBuilderLocalStorage, SettingsFactory exported for creating `splits` property of `storage` object
 export default { ClientFactory, SplitCacheLocalStorage, KeyBuilderLocalStorage, SettingsFactory };
