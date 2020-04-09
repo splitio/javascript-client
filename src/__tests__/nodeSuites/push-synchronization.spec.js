9f9605b47a5559e551e30daaa5a13226a1089fd3
Emiliano Sanchez <emiliano.sanchez@split.io>
2020-04-09 18:39:06 -0300

    testSynchronization for node

--- src/__tests__/nodeSuites/push-synchronization.spec.js
+++ src/__tests__/nodeSuites/push-synchronization.spec.js
@@ -1,12 +1,16 @@
 import splitChangesMock1 from '../mocks/splitchanges.since.-1.json';
 import splitChangesMock2 from '../mocks/splitchanges.since.1457552620999.json';
-import splitChangesMock3 from '../mocks/splitchanges.since.1457552620999.till.1457552631000.json';
-import newSplitUpdateMessage from '../mocks/message.SPLIT_UPDATE.1457552631000.json';
+import splitChangesMock3 from '../mocks/splitchanges.since.1457552620999.till.1457552631000.SPLIT_UPDATE.json';
+import splitChangesMock4 from '../mocks/splitchanges.since.1457552631000.till.1457552650000.SPLIT_KILL.json';
+
+import splitUpdateMessage from '../mocks/message.SPLIT_UPDATE.1457552631000.json';
 import oldSplitUpdateMessage from '../mocks/message.SPLIT_UPDATE.1457552620999.json';
-import authPushEnabled from '../mocks/auth.pushEnabled.facundo@split.io';
+import segmentUpdateMessage from '../mocks/message.SEGMENT_UPDATE.1457552640000.json';
+import splitKillMessage from '../mocks/message.SPLIT_KILL.1457552650000.json';
+
+import authPushEnabled from '../mocks/auth.pushEnabled.node.json';
 
 import { nearlyEqual } from '../utils';
-
 
 import EventSourceMock, { setMockListener } from '../../sync/__tests__/mocks/eventSourceMock';
 import { __setEventSource } from '../../services/getEventSource/node';
@@ -15,9 +19,7 @@ __setEventSource(EventSourceMock);
 import { SplitFactory } from '../../index';
 import SettingsFactory from '../../utils/settings';
 
-const MILLIS_ERROR_MARGIN = 50;
-
-const key = 'emiliano@split.io';
+const key = 'nicolas@split.io';
 
 const baseUrls = {
   sdk: 'https://sdk.baseurl/api',
@@ -28,12 +30,6 @@ const config = {
   core: {
     authorizationKey: '<fake-token-push-1>'
   },
-  scheduler: {
-    featuresRefreshRate: 3000,
-    segmentsRefreshRate: 3000,
-    metricsRefreshRate: 3000,
-    impressionsRefreshRate: 3000
-  },
   urls: baseUrls,
   startup: {
     eventsFirstPushWindow: 3000
@@ -43,40 +39,65 @@ const config = {
 };
 const settings = SettingsFactory(config);
 
+const MILLIS_ERROR_MARGIN = 50;
+const MILLIS_SSE_OPEN = 100;
+const MILLIS_FIRST_SPLIT_UPDATE_EVENT = 200;
+const MILLIS_SECOND_SPLIT_UPDATE_EVENT = 300;
+const MILLIS_SEGMENT_UPDATE_EVENT = 400;
+const MILLIS_SPLIT_KILL_EVENT = 500;
+const MILLIS_DESTROY = 600;
+
 /**
  * Sequence of calls:
  *  0.0 secs: initial SyncAll (/splitChanges, /segmentChanges/*), auth, SSE connection
- *  0.5 sects: SSE connection opened -> syncAll (/splitChanges, /segmentChanges/*)
- *  1.0 secs: new SPLIT_UPDATE event -> /splitChanges
- *  1.5 sects: old SPLIT_UPDATE event
+ *  0.1 sects: SSE connection opened -> syncAll (/splitChanges, /segmentChanges/*)
+ *  0.2 secs: SPLIT_UPDATE event -> /splitChanges
+ *  0.3 sects: SPLIT_UPDATE event with old changeNumber
+ *  0.4 secs: SEGMENT_UPDATE event -> /segmentChanges/
+ *  0.5 secs: SPLIT_KILL event -> /splitChanges
  */
-export function testSplitUpdate(mock, assert) {
-
-  const MILLIS_SSE_OPEN = 500;
-  const MILLIS_FIRST_SPLIT_UPDATE_EVENT = 1000;
-  const MILLIS_SECOND_SPLIT_UPDATE_EVENT = 1500;
+export function testSynchronization(mock, assert) {
 
   const start = Date.now();
 
   const splitio = SplitFactory(config);
   const client = splitio.client();
 
+  // mock SSE open and message events
   setMockListener(function (eventSourceInstance) {
     setTimeout(() => {
       eventSourceInstance.emitOpen();
-    }, MILLIS_SSE_OPEN); // open SSE connection after 0.5 seconds
+    }, MILLIS_SSE_OPEN); // open SSE connection after 0.1 seconds
     setTimeout(() => {
-      eventSourceInstance.emitMessage(newSplitUpdateMessage);
-    }, MILLIS_FIRST_SPLIT_UPDATE_EVENT); // send a SPLIT_UPDATE event with a new changeNumber after 1 seconds
+      assert.equal(client.getTreatment(key, 'whitelist'), 'not_allowed', 'evaluation of initial Split');
+      client.once(client.Event.SDK_UPDATE, () => {
+        assert.equal(client.getTreatment(key, 'whitelist'), 'allowed', 'evaluation of updated Split');
+      });
+      eventSourceInstance.emitMessage(splitUpdateMessage);
+    }, MILLIS_FIRST_SPLIT_UPDATE_EVENT); // send a SPLIT_UPDATE event with a new changeNumber after 0.2 seconds
     setTimeout(() => {
       eventSourceInstance.emitMessage(oldSplitUpdateMessage);
-    }, MILLIS_SECOND_SPLIT_UPDATE_EVENT); // send an old SPLIT_UPDATE event with an old changeNumber after 1 seconds
+    }, MILLIS_SECOND_SPLIT_UPDATE_EVENT); // send a SPLIT_UPDATE event with an old changeNumber after 0.3 seconds
     setTimeout(() => {
-      assert.equal(client.getTreatment(key, 'whitelist'), 'allowed', 'treatment value of updated Split');
+      assert.equal(client.getTreatment(key, 'qc_team'), 'yes', 'evaluation with initial segment');
+      client.once(client.Event.SDK_UPDATE, () => {
+        assert.equal(client.getTreatment(key, 'qc_team'), 'no', 'evaluation with updated segment');
+      });
+      eventSourceInstance.emitMessage(segmentUpdateMessage);
+    }, MILLIS_SEGMENT_UPDATE_EVENT); // send a SEGMENT_UPDATE event with a new changeNumber after 0.4 seconds
+    setTimeout(() => {
+      assert.equal(client.getTreatment(key, 'whitelist'), 'allowed', 'evaluation with not killed Split');
+      client.once(client.Event.SDK_UPDATE, () => {
+        assert.equal(client.getTreatment(key, 'whitelist'), 'not_allowed', 'evaluation with killed Split');
+      });
+      eventSourceInstance.emitMessage(splitKillMessage);
+    }, MILLIS_SPLIT_KILL_EVENT); // send a SPLIT_KILL event with a new changeNumber after 0.5 seconds
+    setTimeout(() => {
       client.destroy().then(() => {
+        assert.equal(client.getTreatment(key, 'whitelist'), 'control', 'evaluation returns control if client is destroyed');
         assert.end();
       });
-    }, MILLIS_SECOND_SPLIT_UPDATE_EVENT + 500);
+    }, MILLIS_DESTROY); // destroy client after 0.6 seconds
   });
 
   mock.onGet(settings.url('/auth')).replyOnce(function (request) {
@@ -84,25 +105,50 @@ export function testSplitUpdate(mock, assert) {
     assert.pass('auth success');
     return [200, authPushEnabled];
   });
-  mock.onGet(new RegExp(`${settings.url('/segmentChanges/')}.*`)).reply(200, { since: 10, till: 10, name: 'segmentName', added: [], removed: [] });
 
+  // initial split and segment sync
   mock.onGet(settings.url('/splitChanges?since=-1')).replyOnce(function () {
-    assert.equal(client.getTreatment(key, 'whitelist'), 'control', 'control treatment value, since SDK is not ready');
     const lapse = Date.now() - start;
     assert.true(nearlyEqual(lapse, 0, MILLIS_ERROR_MARGIN), 'initial sync');
     return [200, splitChangesMock1];
   });
+  mock.onGet(settings.url('/segmentChanges/employees?since=-1')).replyOnce(
+    200, { since: -1, till: 1457552620999, name: 'employees', added: [key], removed: [] }
+  );
+
+  // split and segment sync after SSE opened
   mock.onGet(settings.url('/splitChanges?since=1457552620999')).replyOnce(function () {
-    assert.equal(client.getTreatment(key, 'whitelist'), 'not_allowed', 'treatment value of initial Split');
     const lapse = Date.now() - start;
-    assert.true(nearlyEqual(lapse, MILLIS_SSE_OPEN, MILLIS_ERROR_MARGIN), 'sync due to success SSE connection');
+    assert.true(nearlyEqual(lapse, MILLIS_SSE_OPEN, MILLIS_ERROR_MARGIN), 'sync after SSE connection is opened');
     return [200, splitChangesMock2];
   });
+  mock.onGet(settings.url('/segmentChanges/employees?since=1457552620999')).replyOnce(
+    200, { since: 1457552620999, till: 1457552620999, name: 'employees', added: [], removed: [] }
+  );
+  // extra reply due to double request. @TODO: remove once `SplitChangesUpdaterFactory` and `segmentChangesFetcher` are updated
+  mock.onGet(settings.url('/segmentChanges/employees?since=1457552620999')).replyOnce(
+    200, { since: 1457552620999, till: 1457552620999, name: 'employees', added: [], removed: [] }
+  );
+
+  // fetch due to SPLIT_UPDATE event
   mock.onGet(settings.url('/splitChanges?since=1457552620999')).replyOnce(function () {
-    assert.equal(client.getTreatment(key, 'whitelist'), 'not_allowed', 'treatment value of initial Split');
     const lapse = Date.now() - start;
-    assert.true(nearlyEqual(lapse, MILLIS_FIRST_SPLIT_UPDATE_EVENT, MILLIS_ERROR_MARGIN), 'sync due to first SPLIT_UPDATE event');
+    assert.true(nearlyEqual(lapse, MILLIS_FIRST_SPLIT_UPDATE_EVENT, MILLIS_ERROR_MARGIN), 'sync due to SPLIT_UPDATE event');
     return [200, splitChangesMock3];
   });
 
+  // fetch due to SEGMENT_UPDATE event
+  mock.onGet(settings.url('/segmentChanges/employees?since=1457552620999')).replyOnce(function () {
+    const lapse = Date.now() - start;
+    assert.true(nearlyEqual(lapse, MILLIS_SEGMENT_UPDATE_EVENT, MILLIS_ERROR_MARGIN), 'sync due to SEGMENT_UPDATE event');
+    return [200, { since: 1457552620999, till: 1457552640000, name: 'employees', added: [], removed: [key] }];
+  });
+
+  // fetch due to SPLIT_KILL event
+  mock.onGet(settings.url('/splitChanges?since=1457552631000')).replyOnce(function () {
+    assert.equal(client.getTreatment(key, 'whitelist'), 'not_allowed', 'evaluation with split killed immediately, before fetch is done');
+    const lapse = Date.now() - start;
+    assert.true(nearlyEqual(lapse, MILLIS_SPLIT_KILL_EVENT, MILLIS_ERROR_MARGIN), 'sync due to SPLIT_KILL event');
+    return [200, splitChangesMock4];
+  });
 }
