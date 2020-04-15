fed90cd26f0f29d92261e391f8cb4649be9e9aae
Emiliano Sanchez <emiliano.sanchez@split.io>
2020-04-15 13:07:31 -0300

    updated notification keeper. refactored notification processor and removed unnecesary params and methods

---
+++ src/sync/NotificationProcessor/NotificationKeeper.js
@@ -0,0 +1,29 @@
+import { PushEventTypes } from '../constants';
+
+const controlPriMatcher = /control_pri$/;
+
+export default function notificationKeeperFactory(feedbackLoopEmitter) {
+
+  let isStreamingUp;
+
+  return {
+    handleOpen() {
+      isStreamingUp = true;
+      feedbackLoopEmitter.emit(PushEventTypes.PUSH_CONNECT);
+    },
+
+    handleIncomingPresenceEvent(eventData, channel) {
+      // logic of NotificationManagerKeeper
+      if (controlPriMatcher.test(channel)) {
+        if (eventData.metrics.publishers === 0 && isStreamingUp) {
+          isStreamingUp = false;
+          feedbackLoopEmitter.emit(PushEventTypes.PUSH_DISCONNECT); // notify(STREAMING_DOWN) in spec
+        }
+        if (eventData.metrics.publishers !== 0 && !isStreamingUp) {
+          isStreamingUp = true;
+          feedbackLoopEmitter.emit(PushEventTypes.PUSH_CONNECT); // notify(STREAMING_UP) in spec
+        }
+      }
+    }
+  };
+}
