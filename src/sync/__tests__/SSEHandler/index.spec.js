7878c7465a3b375055d94c6aa38ce2a31bd0509c
Emiliano Sanchez <emiliano.sanchez@split.io>
2020-04-20 15:51:41 -0300

    try-catch when parsing data of SSE errors and messages.

--- src/sync/__tests__/SSEHandler/index.spec.js
+++ src/sync/__tests__/SSEHandler/index.spec.js
@@ -1,7 +1,7 @@
 import tape from 'tape';
 import sinon from 'sinon';
 import SSEHandlerFactory from '../../SSEHandler';
-import { PUSH_CONNECT } from '../../constants';
+import { PUSH_CONNECT, SSE_ERROR } from '../../constants';
 
 tape('SSEHandler', t => {
 
@@ -11,15 +11,31 @@ tape('SSEHandler', t => {
   const sseHandler = SSEHandlerFactory(pushEmitter);
 
   t.test('handleOpen (NotificationKeeper)', assert => {
+    pushEmitter.emit.resetHistory();
     sseHandler.handleOpen();
-    assert.true(pushEmitter.emit.calledOnceWithExactly(PUSH_CONNECT), '...');
+    assert.true(pushEmitter.emit.calledOnceWithExactly(PUSH_CONNECT), 'must emit PUSH_CONNECT');
 
     assert.end();
   });
 
-  // t.test('handlerMessage (NotificationProcessor)', assert => {
+  t.test('handleError', assert => {
+    pushEmitter.emit.resetHistory();
 
-  //   assert.end();
-  // });
+    const error = 'some error';
+    sseHandler.handleError(error);
+    assert.true(pushEmitter.emit.calledOnceWithExactly(SSE_ERROR, error), 'must emit SSE_ERROR with given error');
+
+    const errorWithData = { data: '{ "message": "error message"}' };
+    sseHandler.handleError(errorWithData);
+    assert.true(pushEmitter.emit.calledWithExactly(SSE_ERROR,
+      { ...errorWithData, parsedData: JSON.parse(errorWithData.data) }), 'must emit SSE_ERROR with given error and parsed data');
+
+    assert.end();
+  });
+
+  t.test('handlerMessage (NotificationProcessor)', assert => {
+
+    assert.end();
+  });
 
 });
