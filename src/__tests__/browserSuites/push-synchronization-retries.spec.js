3efe81e71f1e6d46b6df151b13400ab88b93ef43
Emiliano Sanchez <emiliano.sanchez@split.io>
2020-04-22 22:37:17 -0300

    fixed lint issue

--- src/__tests__/browserSuites/push-synchronization-retries.spec.js
+++ src/__tests__/browserSuites/push-synchronization-retries.spec.js
@@ -7,7 +7,6 @@ import mySegmentsNicolasMock2 from '../mocks/mysegments.nicolas@split.io.mock2.j
 import splitUpdateMessage from '../mocks/message.SPLIT_UPDATE.1457552631000.json';
 import oldSplitUpdateMessage from '../mocks/message.SPLIT_UPDATE.1457552620999.json';
 import mySegmentsUpdateMessage from '../mocks/message.MY_SEGMENTS_UPDATE.nicolas@split.io.1457552640000.json';
-import secondMySegmentsUpdateMessage from '../mocks/message.MY_SEGMENTS_UPDATE.nicolas@split.io.1457552641000.json';
 import splitKillMessage from '../mocks/message.SPLIT_KILL.1457552650000.json';
 
 import authPushEnabledNicolas from '../mocks/auth.pushEnabled.nicolas@split.io.json';
