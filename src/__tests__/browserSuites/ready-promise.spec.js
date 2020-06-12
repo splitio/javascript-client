816e56c368c06e953c004370593c23f7ac4a5f6f
Emiliano Sanchez <emiliano.sanchez@split.io>
2020-06-12 15:00:42 -0300

    some polish

--- src/__tests__/browserSuites/ready-promise.spec.js
+++ src/__tests__/browserSuites/ready-promise.spec.js
@@ -1,5 +1,5 @@
 import sinon from 'sinon';
-import { nearlyEqual } from '../testUtils/index';
+import { nearlyEqual } from '../testUtils';
 
 function fromSecondsToMillis(n) {
   return Math.round(n * 1000);
