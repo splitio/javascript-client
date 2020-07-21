60b207a7a81eee9eba2244f35e530c681725638d
Emiliano Sanchez <emiliano.sanchez@split.io>
2020-07-21 13:54:00 -0300

    Map implementation based on popular polyfill

--- src/utils/lang/Maps.js
+++ src/utils/lang/Maps.js
@@ -1,76 +1,73 @@
 /**
- * Map implementation based on JS objects, with the minimal features used by the SDK.
- * Support string type as keys and any object as values
+ * Map implementation based on es6-map polyfill (https://github.com/medikoo/es6-map/blob/master/polyfill.js),
+ * with the minimal features used by the SDK.
  */
-export class ObjectMap {
+export class MapPoly {
 
   // unlike ES6 `Map`, it doesn't accept an iterable as first argument
   constructor() {
-    this.clear();
-  }
-
-  clear() {
-    this.items = {};
+    this.__mapKeysData__ = [];
+    this.__mapValuesData__ = [];
   }
 
   set(key, value) {
-    this.items[key] = value;
+    var index = this.__mapKeysData__.indexOf(key);
+    if (index === -1) {
+      index = this.__mapKeysData__.push(key) - 1;
+    }
+    this.__mapValuesData__[index] = value;
     return this;
   }
 
   get(key) {
-    return this.items[key];
+    var index = this.__mapKeysData__.indexOf(key);
+    if (index === -1) return;
+    return this.__mapValuesData__[index];
   }
 
   has(key) {
-    return this.items[key] !== undefined;
+    return this.__mapKeysData__.indexOf(key) !== -1;
   }
 
-  // unlike `Map.prototype.delete`, it doesn't return a boolean indicating if the item was deleted or not
   delete(key) {
-    delete this.items[key];
+    var index = this.__mapKeysData__.indexOf(key);
+    if (index === -1) return false;
+    this.__mapKeysData__.splice(index, 1);
+    this.__mapValuesData__.splice(index, 1);
+    return true;
   }
-
-  keys() {
-    return Object.keys(this.items);
-  }
-
-  /** Not used feature */
-  // forEach(callback, thisArg) {
-  //   return Object.keys(this.items).forEach(key => callback.call(thisArg, key, this.items[key]));
-  // }
 
   get size() {
-    return Object.keys(this.items).length;
+    return this.__mapKeysData__.length;
   }
 
 }
 
 export function mapKeysToArray(map) {
-  if (map instanceof ObjectMap) {
-    return Object.keys(map.items);
+  if (map instanceof MapPoly) {
+    return map.__mapKeysData__.slice();
   }
+  // if not using MapPoly as map, it means both Map.prototype.keys and Array.from are supported
   return Array.from(map.keys());
 }
 
 export function mapValuesToArray(map) {
-  if (map instanceof ObjectMap) {
-    return Object.keys(map.items).map(key => map.items[key]);
+  if (map instanceof MapPoly) {
+    return map.__mapValuesData__.slice();
   }
-  // if not using ObjectMap as map, it means both Map and Array.from are supported
+  // if not using MapPoly as map, it means both Map.prototype.values and Array.from are supported
   return Array.from(map.values());
 }
 
 /**
- * return the Map constructor to use. If native Map is not available or it doesn't support the required features,
+ * return the Map constructor to use. If `Array.from` built-in or native Map is not available or it doesn't support the required features,
  * a ponyfill with minimal features is returned instead.
  */
-// @TODO review check condition
-function getMap() {
-  if(!Array.from || !Map) { //} || new Map(['key', 'value']).size === 0) {
-    return ObjectMap;
+function getMapConstructor() {
+  if (!Array.from || !Map || !Map.prototype.values || !Map.prototype.keys) {
+    return MapPoly;
   }
   return Map;
 }
 
-export const _Map = getMap();
+export const _Map = getMapConstructor();
