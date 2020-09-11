16e3dbb17721302d15159b728cb96b17cf29973d
Matias Melograno <matiasmelograno@split.io>
2020-09-11 12:45:14 -0300

    removed none

--- src/utils/settings/impressionsMode.js
+++ src/utils/settings/impressionsMode.js
@@ -1,9 +1,9 @@
-import { DEBUG, OPTIMIZED, NONE } from '../constants';
+import { DEBUG, OPTIMIZED } from '../constants';
 import logFactory from '../logger';
 const log = logFactory('splitio-settings');
 
 function validImpressionsMode(impressionsMode) {
-  if ([DEBUG, OPTIMIZED, NONE].indexOf(impressionsMode) === -1) {
+  if ([DEBUG, OPTIMIZED].indexOf(impressionsMode) === -1) {
     log.error('You passed an invalid impressionsMode, impressionsMode should be one of the following values: OPTIMIZED or DEBUG. The sdk continues in OPTIMIZED mode.');
     impressionsMode = OPTIMIZED;
   }
