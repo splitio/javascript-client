a652e85687775614faf27f9d917e3332d72138df
Matias Melograno <matiasmelograno@split.io>
2020-09-14 20:30:32 -0300

    fixed import

--- src/impressions/hasher/__tests__/browser.spec.js
+++ src/impressions/hasher/__tests__/browser.spec.js
@@ -1,5 +1,5 @@
 import tape from 'tape-catch';
-import hasher from '../browser';
+import { hashImpression32 } from '../browser';
 
 tape('Hasher 32 / Impression Hasher Works', assert => {
   const imp1 = {
@@ -18,31 +18,31 @@ tape('Hasher 32 / Impression Hasher Works', assert => {
     label: 'someLabel',
     treatment: 'someTreatment',
   };
-  assert.equal(hasher.hashImpression32(imp1), hasher.hashImpression32(imp2));
+  assert.equal(hashImpression32(imp1), hashImpression32(imp2));
 
   // Different feature
   imp2.feature = 'someOtherFeature';
-  assert.notEqual(hasher.hashImpression32(imp1), hasher.hashImpression32(imp2));
+  assert.notEqual(hashImpression32(imp1), hashImpression32(imp2));
 
   // Different key
   imp2.feature = imp1.feature;
   imp2.keyName = 'someOtherKey';
-  assert.notEqual(hasher.hashImpression32(imp1), hasher.hashImpression32(imp2));
+  assert.notEqual(hashImpression32(imp1), hashImpression32(imp2));
 
   // Different changeNumber
   imp2.keyName = imp1.keyName;
   imp2.changeNumber = 456;
-  assert.notEqual(hasher.hashImpression32(imp1), hasher.hashImpression32(imp2));
+  assert.notEqual(hashImpression32(imp1), hashImpression32(imp2));
 
   // Different label
   imp2.changeNumber = imp1.changeNumber;
   imp2.label = 'someOtherLabel';
-  assert.notEqual(hasher.hashImpression32(imp1), hasher.hashImpression32(imp2));
+  assert.notEqual(hashImpression32(imp1), hashImpression32(imp2));
 
   // Different Treatment
   imp2.label = imp1.label;
   imp2.treatment = 'someOtherTreatment';
-  assert.notEqual(hasher.hashImpression32(imp1), hasher.hashImpression32(imp2));
+  assert.notEqual(hashImpression32(imp1), hashImpression32(imp2));
 
   assert.end();
 });
@@ -57,18 +57,18 @@ tape('Hasher 32 / Impression Hasher Does Not Crash', assert => {
   };
 
   imp1.keyName = null;
-  assert.isNot(hasher.hashImpression32(imp1), null);
+  assert.isNot(hashImpression32(imp1), null);
 
   imp1.changeNumber = null;
-  assert.isNot(hasher.hashImpression32(imp1), null);
+  assert.isNot(hashImpression32(imp1), null);
 
   imp1.label = null;
-  assert.isNot(hasher.hashImpression32(imp1), null);
+  assert.isNot(hashImpression32(imp1), null);
 
   imp1.treatment = null;
-  assert.isNot(hasher.hashImpression32(imp1), null);
+  assert.isNot(hashImpression32(imp1), null);
 
-  assert.is(hasher.hashImpression32(null), null);
+  assert.is(hashImpression32(null), null);
 
   assert.end();
 });
