d2428ba03af8540e9ddb0a6f77d337cbfcff9eb8
NicoZelaya <zelayadn@gmail.com>
2020-11-04 17:48:09 -0300

    Updating imports to be relative

--- src/impressions/hasher/hashImpression128.js
+++ src/impressions/hasher/hashImpression128.js
@@ -1,5 +1,5 @@
 import { hash128 } from '../../engine/engine/murmur3/murmur3_128';
-import { buildKey } from '.';
+import { buildKey } from './';
 
 export function hashImpression128(impression) {
   return impression ? hash128(buildKey(impression)).toString() : null;
