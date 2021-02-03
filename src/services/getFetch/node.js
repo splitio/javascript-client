65a342c97d8f60354f892db6e83b250f83e75c84
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-02-03 19:44:57 -0300

    updated how node-fetch issue is handled, and polished README.md

--- src/services/getFetch/node.js
+++ src/services/getFetch/node.js
@@ -1,4 +1,7 @@
-let nodeFetch = require('node-fetch').default;
+let nodeFetch = require('node-fetch');
+
+// Handle node-fetch issue https://github.com/node-fetch/node-fetch/issues/1037
+if(typeof nodeFetch !== 'function') nodeFetch = nodeFetch.default;
 
 // This function is only exposed for testing purposes.
 export function __setFetch(fetch) {
