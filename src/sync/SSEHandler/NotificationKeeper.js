9c8d9fd6c97eed77679eb720d3f55462bfd30a41
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-03-09 13:05:18 -0300

    feedback

--- src/sync/SSEHandler/NotificationKeeper.js
+++ src/sync/SSEHandler/NotificationKeeper.js
@@ -37,17 +37,17 @@ export default function notificationKeeperFactory(feedbackLoopEmitter) {
           if (timestamp > c.oTime) {
             c.oTime = timestamp;
             c.hasPublishers = publishers !== 0;
-            const newHasPublishers = getHasPublishers();
+            const hasPublishersNow = getHasPublishers();
             if (hasResumed) {
-              if (!newHasPublishers && hasPublishers) {
+              if (!hasPublishersNow && hasPublishers) {
                 feedbackLoopEmitter.emit(PUSH_SUBSYSTEM_DOWN);
-              } else if (newHasPublishers && !hasPublishers) {
+              } else if (hasPublishersNow && !hasPublishers) {
                 feedbackLoopEmitter.emit(PUSH_SUBSYSTEM_UP);
               }
               // nothing to do when hasResumed === false:
-              // streaming is already down for `!newHasPublishers`, and cannot be up for `newHasPublishers`
+              // streaming is already down for `!hasPublishersNow`, and cannot be up for `hasPublishersNow`
             }
-            hasPublishers = newHasPublishers;
+            hasPublishers = hasPublishersNow;
           }
           return;
         }
