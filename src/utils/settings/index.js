f23b02479c756de29d5b22e2ca0f4dd0e2e10fdb
NicoZelaya <zelayadn@gmail.com>
2021-05-04 20:38:51 -0300

    update version in utils

--- src/utils/settings/index.js
+++ src/utils/settings/index.js
@@ -27,7 +27,7 @@ import { API } from '../../utils/logger';
 import { STANDALONE_MODE, STORAGE_MEMORY, CONSUMER_MODE, OPTIMIZED } from '../../utils/constants';
 import validImpressionsMode from './impressionsMode';
 
-const version = '10.15.5';
+const version = '10.15.6-canary.1';
 const eventsEndpointMatcher = /^\/(testImpressions|metrics|events)/;
 const authEndpointMatcher = /^\/auth/;
 const streamingEndpointMatcher = /^\/(sse|event-stream)/;
