b91afb458c79a86c4f6c850b4dc9a83d887ebc63
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-08-05 23:56:17 -0300

    making more flexible the access to global fetch and EventSource objects for Node and Browser

--- src/services/getEventSource/browser.js
+++ src/services/getEventSource/browser.js
@@ -1,4 +1,3 @@
 export default function getEventSource() {
-  // eslint-disable-next-line compat/compat
-  return typeof window !== 'undefined' && typeof window.EventSource === 'function' ? window.EventSource : undefined;
+  return typeof EventSource === 'function' ? EventSource : undefined;
 }
