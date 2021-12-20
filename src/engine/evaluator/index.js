3e5fa33c8928d3703f9d0cb17faa6f093358dd72
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-12-20 14:29:56 -0300

    fix: prevent returning null dynamic config if treatment name contains a "."

--- src/engine/evaluator/index.js
+++ src/engine/evaluator/index.js
@@ -17,7 +17,6 @@ limitations under the License.
 import Engine from '../';
 import thenable from '../../utils/promise/thenable';
 import * as LabelsConstants from '../../utils/labels';
-import { get } from '../../utils/lang';
 import { CONTROL } from '../../utils/constants';
 
 const treatmentException = {
@@ -107,13 +106,13 @@ function getEvaluation(
     if (thenable(evaluation)) {
       return evaluation.then(result => {
         result.changeNumber = split.getChangeNumber();
-        result.config = get(splitJSON, `configurations.${result.treatment}`, null);
+        result.config = splitJSON.configurations && splitJSON.configurations[result.treatment] || null;
 
         return result;
       });
     } else {
       evaluation.changeNumber = split.getChangeNumber(); // Always sync and optional
-      evaluation.config = get(splitJSON, `configurations.${evaluation.treatment}`, null);
+      evaluation.config = splitJSON.configurations && splitJSON.configurations[evaluation.treatment] || null;
     }
   }
 
