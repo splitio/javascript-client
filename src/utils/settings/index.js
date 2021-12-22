77506b37bbc2f6e7d9a4824962dfb82cb9a3f14c
Emiliano Sanchez <emiliano.sanchez@split.io>
2021-12-22 16:34:19 -0300

    prepare stable release

--- src/utils/settings/index.js
+++ src/utils/settings/index.js
@@ -27,7 +27,7 @@ import { API } from '../../utils/logger';
 import { STANDALONE_MODE, STORAGE_MEMORY, CONSUMER_MODE, OPTIMIZED } from '../../utils/constants';
 import validImpressionsMode from './impressionsMode';
 
-const version = '10.16.2-rc.0';
+const version = '10.17.0';
 const eventsEndpointMatcher = /^\/(testImpressions|metrics|events)/;
 const authEndpointMatcher = /^\/v2\/auth/;
 const streamingEndpointMatcher = /^\/(sse|event-stream)/;
