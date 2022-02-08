e2f2734dff1b954ef93d41e896e4f2dae9ef2c94
Emiliano Sanchez <emiliano.sanchez@split.io>
2022-02-08 15:07:37 -0300

    testing

---
+++ types/client/index.d.ts
@@ -0,0 +1,14 @@
+// Declaration file for Javascript Split Software SDK
+// Project: http://www.split.io/
+
+/// <reference path="../splitio.d.ts" />
+export = JsSdk;
+
+declare module JsSdk {
+  /**
+   * Split.io sdk factory function.
+   * The settings parameter should be an object that complies with the SplitIO.IBrowserSettings.
+   * For more information read the corresponding article: @see {@link https://help.split.io/hc/en-us/articles/360020448791-JavaScript-SDK#configuration}
+   */
+  export function SplitFactory(settings: SplitIO.IBrowserSettings): SplitIO.IBrowserSDK;
+}
