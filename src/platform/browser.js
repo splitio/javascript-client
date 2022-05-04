7b264bff770e224109bdd91ac42bf5b9636d444b
Emiliano Sanchez <emiliano.sanchez@split.io>
2022-05-04 17:27:33 -0300

    update platform object with now function

--- src/platform/browser.js
+++ src/platform/browser.js
@@ -2,11 +2,13 @@ import { EventEmitter } from './EventEmitter';
 import { getFetch } from '../platform/getFetch/browser';
 import { getEventSource } from '../platform/getEventSource/browser';
 import { BrowserSignalListener } from '@splitsoftware/splitio-commons/src/listeners/browser';
+import { now } from '@splitsoftware/splitio-commons/src/utils/timeTracker/now/browser';
 
 export const platform = {
   getFetch,
   getEventSource,
-  EventEmitter
+  EventEmitter,
+  now
 };
 
 export const SignalListener = BrowserSignalListener;
