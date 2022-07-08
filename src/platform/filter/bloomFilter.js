edfd12e2ed5cd9fe6af200e1d6d39ff19212e67a
Emmanuel Zamora <emmanuel-zamora@split.io>
2022-07-08 15:46:49 -0300

    Update test file name. var improvements

--- src/platform/filter/bloomFilter.js
+++ src/platform/filter/bloomFilter.js
@@ -1,31 +1,25 @@
 import { BloomFilter } from '@ably/bloomit';
 
-export function getBloomFilter(spectedInsertions, errorRate) {
-  let __spectedInsertions = 10000000; // 10.000.000 default size
-  let __errorRate = 0.01; // 0,01 default error rate
-  let __filter;
-  
-  if (spectedInsertions) __spectedInsertions = spectedInsertions;
-  if (errorRate) __errorRate = errorRate;
-  __filter = BloomFilter.create(__spectedInsertions, __errorRate);
+export function getBloomFilter(expectedInsertions = 10000000, errorRate = 0.01) {
+  let filter = BloomFilter.create(expectedInsertions, errorRate);
   
   return {  
   
-    add: (data) => {
+    add(data) {
       try {
-        __filter.add(data);
+        filter.add(data);
         return true;
       } catch(error){
         return false;
       }
     },
   
-    contains: (data) => {
-      return __filter.has(data);
+    contains(data) {
+      return filter.has(data);
     },
   
-    clear: () => {
-      __filter = BloomFilter.create(__spectedInsertions, __errorRate);
+    clear() {
+      filter = BloomFilter.create(expectedInsertions, errorRate);
     }
   
   };
