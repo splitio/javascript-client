7895ee6f6f0d06b382238d902024f07931b5f410
Emiliano Sanchez <emiliano.sanchez@split.io>
2022-07-20 18:04:54 -0300

    restore ci-cd

--- src/sync/offline/LocalhostFromFile.js
+++ src/sync/offline/LocalhostFromFile.js
@@ -2,7 +2,7 @@ import { splitsParserFromFileFactory } from './splitsParserFromFile';
 import { syncManagerOfflineFactory } from '@splitsoftware/splitio-commons/src/sync/offline/syncManagerOffline';
 
 // Singleton instance of the factory function for offline SyncManager from YAML file (a.k.a. localhostFromFile)
-// Requires Node 'fs' and 'path' APIs.
+// It uses NodeJS APIs.
 const localhostFromFile = syncManagerOfflineFactory(splitsParserFromFileFactory);
 localhostFromFile.type = 'LocalhostFromFile';
 
