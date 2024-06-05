a3658088dd5cb8534582357881f4f223dd8558be
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-06-05 12:30:59 -0300

    Update eventsource to accept an agent option

--- src/platform/getEventSource/eventsource.js
+++ src/platform/getEventSource/eventsource.js
@@ -1,5 +1,32 @@
 /* eslint-disable no-prototype-builtins */
 /* eslint-disable no-restricted-syntax */
+/*
+Modified version of "eventsource" v1.1.2 package (https://www.npmjs.com/package/eventsource/v/1.1.2)
+that accepts a custom agent.
+
+The MIT License
+
+Copyright (c) EventSource GitHub organisation
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 var parse = require('url').parse;
 var events = require('events');
 var https = require('https');
@@ -105,6 +132,11 @@ function EventSource(url, eventSourceInitDict) {
 
     if (eventSourceInitDict && eventSourceInitDict.createConnection !== undefined) {
       options.createConnection = eventSourceInitDict.createConnection;
+    }
+
+    // If specify agent, use it.
+    if (eventSourceInitDict && eventSourceInitDict.agent !== undefined) {
+      options.agent = eventSourceInitDict.agent;
     }
 
     // If specify http proxy, make the request to sent to the proxy server,
