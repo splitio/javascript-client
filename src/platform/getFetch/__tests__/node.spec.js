63d7f6636657bbe032fb0e25a923d417bc708069
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-06-11 13:47:27 -0300

    Add getOptions for NodeJS

--- src/platform/getFetch/__tests__/node.spec.js
+++ src/platform/getFetch/__tests__/node.spec.js
@@ -1,33 +1,8 @@
 import tape from 'tape-catch';
-import sinon from 'sinon';
-import { getFetch, __setFetch } from '../node';
+import { getFetch } from '../node';
 
-tape('getFetch returns a wrapped node-fetch module in Node', assert => {
-  assert.equal(typeof getFetch(), 'function');
-
-  assert.end();
-});
-
-tape('getFetch passes an agent object to HTTPs requests', assert => {
-  const fetchMock = sinon.stub();
-  __setFetch(fetchMock);
-
-  const fetch = getFetch();
-
-  fetch('http://test.com');
-  assert.true(fetchMock.calledWithExactly('http://test.com', { agent: undefined }));
-
-  fetch('https-https://', { option: 'value' });
-  assert.true(fetchMock.calledWithExactly('https-https://', { option: 'value', agent: undefined }));
-
-  fetch('https://test.com');
-  assert.true(fetchMock.calledWithExactly('https://test.com', { agent: sinon.match.object }));
-
-  fetch('https://test.com', { option: 'value' });
-  assert.true(fetchMock.calledWithExactly('https://test.com', { option: 'value', agent: sinon.match.object }));
-
-  // Restore
-  __setFetch(require('node-fetch'));
+tape('getFetch returns node-fetch module in Node', assert => {
+  assert.equal(getFetch(), require('node-fetch'));
 
   assert.end();
 });
