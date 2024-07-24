28388205668212abcde6ae55319a91e98bad05df
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-07-23 23:16:02 -0300

    Tests for polling

--- src/__tests__/push/browser.spec.js
+++ src/__tests__/push/browser.spec.js
@@ -4,7 +4,7 @@ import { testAuthWithPushDisabled, testAuthWith401, testNoEventSource, testSSEWi
 import { testPushRetriesDueToAuthErrors, testPushRetriesDueToSseErrors, testSdkDestroyWhileAuthRetries, testSdkDestroyWhileAuthSuccess, testSdkDestroyWhileConnDelay } from '../browserSuites/push-initialization-retries.spec';
 import { testSynchronization } from '../browserSuites/push-synchronization.spec';
 import { testSynchronizationRetries } from '../browserSuites/push-synchronization-retries.spec';
-import { testFallbacking } from '../browserSuites/push-fallbacking.spec';
+import { testFallback } from '../browserSuites/push-fallback.spec';
 import { testRefreshToken } from '../browserSuites/push-refresh-token.spec';
 import { testSplitKillOnReadyFromCache } from '../browserSuites/push-corner-cases.spec';
 import { testFlagSets } from '../browserSuites/push-flag-sets.spec';
@@ -32,7 +32,7 @@ tape('## Browser JS - E2E CI Tests for PUSH ##', function (assert) {
   assert.test('E2E / PUSH synchronization: happy paths', testSynchronization.bind(null, fetchMock));
   assert.test('E2E / PUSH synchronization: retries', testSynchronizationRetries.bind(null, fetchMock));
 
-  assert.test('E2E / PUSH fallbacking, CONTROL, OCCUPANCY and STREAMING_RESET messages', testFallbacking.bind(null, fetchMock));
+  assert.test('E2E / PUSH fallback, CONTROL, OCCUPANCY and STREAMING_RESET messages', testFallback.bind(null, fetchMock));
 
   assert.test('E2E / PUSH refresh token and connection delay', testRefreshToken.bind(null, fetchMock));
 
