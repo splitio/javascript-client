df870f452ffe77d7f6d3e47110d750844810590e
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-11-07 15:54:05 -0300

    Re-export TypeScript SplitIO namespace at ./types/splitio.d.ts to allow explicit imports of types

--- src/settings/defaults/version.js
+++ src/settings/defaults/version.js
@@ -1,1 +1,1 @@
-export const packageVersion = '11.0.0';
+export const packageVersion = '11.0.1';
---
+++ types/splitio.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for JavaScript and NodeJS Split Software SDK
+
+import '@splitsoftware/splitio-commons';
+
+export as namespace SplitIO;
+export = SplitIO;
